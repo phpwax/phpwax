86aa995fa04b95f95335befc8ffe95394c36b581
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-08-02 10:36:41 +0000

    minor bug test
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@139 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- framework/lib_core/ApplicationBase.php
+++ framework/lib_core/ApplicationBase.php
@@ -71,10 +71,9 @@ class ApplicationBase
    */
 	function __construct()
 	{
-		//Start a session
-		Session::start();		
     set_exception_handler(array($this, 'process_exception'));    
     set_error_handler(array($this, 'process_error'), 259);
+		Session::start();		
 		$this->load_config();				
 		$this->copy_javascript();		
 		//$this->mysql_db_backup(); 
