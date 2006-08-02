a1f920489f0ee8e73eeeb96ca255c2b170f32324
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-08-02 10:37:26 +0000

    minor bug test
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@140 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- framework/lib_core/ApplicationBase.php
+++ framework/lib_core/ApplicationBase.php
@@ -73,7 +73,7 @@ class ApplicationBase
 	{
     set_exception_handler(array($this, 'process_exception'));    
     set_error_handler(array($this, 'process_error'), 259);
-		Session::start();		
+		if(!Session::start() ) { error_log('no session started');}		
 		$this->load_config();				
 		$this->copy_javascript();		
 		//$this->mysql_db_backup(); 
