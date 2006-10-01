07c2149ee79aad595344483e978db994ca32ceee
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-01 08:00:08 +0000

    Fix to bug in include_path setting
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@193 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 define('WAX_ROOT', dirname(dirname(__FILE__)).'/');
-ini_set('include_path', WAX_ROOT);
+ini_set('include_path', ini_get("include_path").":".WAX_ROOT);
 require_once('wax/AutoLoader.php');
 AutoLoader::run_application();
 ?>
