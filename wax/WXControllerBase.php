c7016a6a8567eda7968b7ec88892425bba1743be
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-17 09:04:05 +0000

    Clean up of stray code, fix to missing controller variable
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@215 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXControllerBase.php
+++ wax/WXControllerBase.php
@@ -13,17 +13,13 @@ abstract class WXControllerBase extends ApplicationBase
 {
   protected $models=array();
   protected $route_array=null;
+	protected $controller;
   protected $action;
   public $use_layout='application';
   public $use_view=null;
   private $class_name='';
   protected $referrer;
-	protected $user_messages=array();
-	protected $user_errors=array();
-	protected $show_errors=true;
-	protected $show_messages=true;
 	protected $use_plugin=false;
-	public $body_js_files=array();
 	
 	/**
 	 *	An array of actions that implement caching, or 'all' to cache entire model.
@@ -40,63 +36,10 @@ abstract class WXControllerBase extends ApplicationBase
    * 	what to do based on the $route_array
    */
    public $accept_routes=0;
-   /*
-    * This array contains the info that is passed on to the layout.
-    * public functions are provided to add variables to this, do not
-    * set directly.
-    */
-   public $layout=array(
-        'styles'=>    array(
-                              
-                           ),
-        'scripts'=>   array(
-                            '/javascript/lib/form.behaviours.js',							
-                           ),
-         'meta'=>     array()
-   );
   
-   function __construct()
-   {      
+	function __construct() {      
       $this->class_name=get_class($this);
       $this->referrer=Session::get('referrer');
-   }
-   
-
-	/**
- 	 *	Adds a custom stylesheet to the layout. main.css is included by default.
-	 *	@access protected
-	 *	@param string $url the location of the file (relative path)
-	 *	@param string $media defaults to all. Options include screen, handheld, print
- 	 */   
-   protected function add_stylesheet($url, $media='all')
-   {
-      $this->layout['styles'][]=array($url, $media);
-      return true;
-   }
-
-	/**
- 	 *	Adds additional javascript src tags to the layout.
-	 *	@access protected
-	 *	@param string $url the location of the javascript
- 	 */
-   protected function add_javascript($url)
-   {
-      $this->layout['scripts'][]=$url;
-      return true;
-   }
-
-	protected function add_body_js($file) {
-		$this->body_js_files[]=$file;
-	}
-
-	/**
- 	 *	Adds custom meta data which are passed onto the template.
-	 *	@access protected
-	 *	@param string $name
-	 *	@param string $content
- 	 */   
-  protected function add_meta_content($name, $content) {
-   	$this->layout['meta'][$name]=$content; return true;   
   }
 
 	/**
@@ -152,19 +95,6 @@ abstract class WXControllerBase extends ApplicationBase
 	}
 
 	/**
- 	 *	Adds a message that will be displayed to the user on the next screen.
-	 *	@access protected
-	 *	@param string $message
- 	 */
-	public function add_user_message($message) {
-		if(Session::add_message($message)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
  	 *	In the abstract class this remains empty. It is overridden by the controller,
 	 *	any commands will be run by all actions prior to running the action.
 	 *	@access protected
@@ -197,10 +127,8 @@ abstract class WXControllerBase extends ApplicationBase
 	 * @return void
 	 **/	
 	function __call($method, $args) {
-		if(!$helperresult) {
-			if(method_exists($this, 'missing_action')) {
-				$this->missing_action(); exit;
-			}
+		if(method_exists($this, 'missing_action')) {
+			$this->missing_action(); exit;
 			throw new WXException("No Action Defined for - ".$this->action, "Missing Action");
 		}
 		exit;
