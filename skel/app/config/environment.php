867f5e2309ef1e9e5359694329d334469cd50622
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-18 16:14:46 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@229 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- skel/app/config/environment.php
+++ skel/app/config/environment.php
@@ -3,13 +3,16 @@
 //define('WAX_EDGE', 'true');
 
 define('WAX_VERSION', '0.6');
-define('WAX_ROOT', dirname(dirname(__FILE__)) );
-ini_set('include_path', ini_get("include_path").":".WAX_ROOT);
+define('WAX_ROOT', dirname(dirname(dirname(__FILE__))) );
 if(defined("WAX_EDGE")) {
-	ini_set('include_path', ini_get("include_path").":/home/waxphp/trunk/");
+	ini_set('include_path', ini_get("include_path").":/home/waxphp/trunk");
+	define('FRAMEWORK_DIR', "/home/waxphp/trunk/wax");
 } else {
 	ini_set('include_path', ini_get("include_path").":/home/waxphp/tags/".WAX_VERSION);
+	define('FRAMEWORK_DIR', "/home/waxphp/trunk/tags/".WAX_VERSION."/wax");
 }
+ini_set('include_path', ini_get("include_path").":".WAX_ROOT);
+
 
 //AutoLoader::add_plugin_directory("cms");
 ?>
