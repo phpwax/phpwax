1794888d2b38325b27797d5500532c24d017cd68
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-18 14:53:40 +0000

    moved to revised repository structure
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@221 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- skel/app/config/environment.php
+++ skel/app/config/environment.php
@@ -1,3 +1,15 @@
 <?
-AutoLoader::add_plugin_directory("cms");
+/* Uncomment this line to always run on the latest nightly build */
+//define('WAX_EDGE', 'true');
+
+define('WAX_VERSION', '0.6');
+define('WAX_ROOT', dirname(dirname(__FILE__)) );
+ini_set('include_path', ini_get("include_path").":".WAX_ROOT);
+if(defined("WAX_EDGE")) {
+	ini_set('include_path', ini_get("include_path").":/home/waxphp/trunk/");
+} else {
+	ini_set('include_path', ini_get("include_path").":/home/waxphp/tags/".WAX_VERSION);
+}
+
+//AutoLoader::add_plugin_directory("cms");
 ?>
--- skel/public/index.php
+++ skel/public/index.php
@@ -1,6 +1,5 @@
 <?php
-define('WAX_ROOT', dirname(dirname(__FILE__)).'/');
-ini_set('include_path', ini_get("include_path").":".WAX_ROOT);
+require_once(dirname(dirname(__FILE__)).'/config/environment.php');
 require_once('wax/AutoLoader.php');
 AutoLoader::run_application();
 ?>
