fab43aebebdc05579f6d0b24e28877b860da09e1
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-19 13:30:11 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@249 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- skel/script/new_controller.php
+++ skel/script/new_controller.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-$cntdir = dirname(__FILE__).'/../app/controller/.';
+$cntdir = dirname(__FILE__).'/../app/controller/';
 $controller_name = $argv[1];
 $content = "<?php
 class {$controller_name}Controller extends ApplicationController
@@ -9,7 +9,7 @@ class {$controller_name}Controller extends ApplicationController
 }
 ?>
 ";
-$command = "echo ".'"'.$content.'"'." > ".$cntdir.$controller_name.".php";
+$command = "echo ".'"'.$content.'"'." > ".$cntdir.$controller_name."Controller.php";
 system($command);
 
 
