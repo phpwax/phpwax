7dcd707e94bbd4122e389c02ea9189df91e41937
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-19 13:52:54 +0000

    new script 'new_test'
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@255 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ skel/script/new_test.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+$testdir = dirname(__FILE__).'/../app/tests/';
+$test_name = ucfirst($argv[1]);
+$content = "<?php
+class Test{$test_name} extends WXTestCase
+{
+	public function setUp() {
+		
+	}
+	
+	public function tearDown() {
+		
+	}
+	
+	/* Add tests below here. all must start with the word 'test'
+}
+?>
+";
+$command = "echo ".'"'.$content.'"'." > ".$testdir."Test".$test_name.".php";
+system($command);
+
+?>
