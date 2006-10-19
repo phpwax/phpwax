ce685f6690abde88ff1609dbffebdff0ec880f5e
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-19 12:39:01 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@236 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- skel/script/freeze_wax.php
+++ skel/script/freeze_wax.php
@@ -5,10 +5,10 @@ require_once dirname(__FILE__).'/../app/config/environment.php';
 if(is_dir(WAX_ROOT."wax")) {
 	echo "Wax is already in your app directory. Delete it to freeze a new version";
 } elseif(defined("WAX_EDGE")) {
-	$command = "svn export svn://svn.webxpress.com/home/SVN/wxframework/trunk ".dirname(__FILE__)."/../wax";
+	$command = "svn export svn://svn.webxpress.com/home/SVN/wxframework/trunk/wax/ ".dirname(__FILE__)."/../wax";
 	system($command);
 } else {
-	$command = "svn export svn://svn.webxpress.com/home/SVN/wxframework/tags/".WAX_VERSION."/ ".dirname(__FILE__)."/../wax";
+	$command = "svn export svn://svn.webxpress.com/home/SVN/wxframework/tags/".WAX_VERSION."/wax/ ".dirname(__FILE__)."/../wax";
 	system($command);
 }
 
