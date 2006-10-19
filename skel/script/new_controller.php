de2c1341da0b7b22e78c878ab6d0abd7584b7b34
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-19 13:41:41 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@252 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- skel/script/new_controller.php
+++ skel/script/new_controller.php
@@ -3,10 +3,12 @@
 $cntdir = dirname(__FILE__).'/../app/controller/';
 $controller_name = $argv[1];
 if(strpos($controller_name, "/")) {
-	$cont = explode($controller_name, "/");
+	$cont = explode("/", $controller_name);
 	$cntdir = $cntdir.$cont[0]."/";
 	system("mkdir -p {$cntdir}");
 	$controller_name = ucfirst($cont[0]).ucfirst($cont[1]);
+} else {
+	$controller_name = ucfirst($argv[1]);
 }
 $content = "<?php
 class {$controller_name}Controller extends ApplicationController
