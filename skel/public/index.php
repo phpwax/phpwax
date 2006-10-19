04529e03fe4d7cdf75a249517004df7932aba7ef
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-19 15:44:19 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@268 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- skel/public/index.php
+++ skel/public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../app/config/environment.php');
+require_once dirname(__FILE__).'/../app/config/environment.php';
 require_once('wax/AutoLoader.php');
 AutoLoader::run_application();
 ?>
