9872c058e3905f84ff8f154af78c1004ae7ec8f9
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-20 09:20:36 +0000

    changes to config layout and a new deploy script
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@272 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ skel/script/deploy.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+require_once dirname(__FILE__).'/../app/config/environment.php';
+require_once 'wax/AutoLoader.php';
+AutoLoader::include_dir(FRAMEWORK_DIR);
+$configFile=APP_DIR.'config/config.yml';
+$config_array = Spyc::YAMLLoad($configFile);
+$deployment_settings = $config_array['deploy'];
+
+$command = "cd ".WAX_ROOT;
+system($command);
+
+$command = "ssh "$deployment_settings['user']."@".$deployment_settings['server'];
+system($command);
+
+?>
