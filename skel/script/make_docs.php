4614609f509b94f9842e35903d19d8666ffb492a
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-23 09:56:44 +0000

    new scripts
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@283 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ skel/script/make_docs.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+require_once dirname(__FILE__).'/../app/config/environment.php';
+$output_dir = WAX_ROOT."doc";
+$source_dir1 = WAX_ROOT."app"
+$source_dir2 = FRAMEWORK_DIR;
+
+$command = "phpdoc -t {$output_dir} -d {$source_dir1},{$source_dir2} -o HTML:frames:phpedit";
+system($command);
+echo "Documentation has been created in /doc. Open the index.html file to view.
+"
+?>
---
+++ skel/script/new_email.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+$modeldir = dirname(__FILE__).'/../app/model/';
+$model_name = ucfirst($argv[1]);
+$content = "<?php
+class {$model_name} extends WXEmail
+{
+	
+}
+?>
+";
+if(is_readable($modeldir.$model_name.".php")) {
+  exit("[ERROR] Not written, a model of that name already exists.
+");
+}
+$command = "echo ".'"'.$content.'"'." > ".$modeldir.$model_name.".php";
+system($command);
+
+?>
