477b6e0ec9e3efc71ce9c8bb7e90342351170dc2
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-24 08:35:35 +0000

    debug
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@293 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXTemplate.php
+++ wax/WXTemplate.php
@@ -29,6 +29,7 @@ class WXTemplate
 		} elseif($this->view_base && is_readable($this->view_base.$pFile)) {
 			$pFile = $this->view_base.$pFile;
 		} elseif($this->plugin_view_path && is_readable($this->plugin_view_path.$pFile)) {
+		  echo "Looking in ".$this->plugin_view_path.$pFile;
 		  $pFile = $this->plugin_view_path.$pFile;
 		} else {
 			$pFile = VIEW_DIR.$pFile;
