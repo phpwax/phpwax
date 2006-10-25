3128699695619b3067e177d0d37a8291f93267b9
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-25 15:50:26 +0000

    continued work on WXMigrate
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@347 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXMigrate.php
+++ wax/WXMigrate.php
@@ -13,14 +13,29 @@ class WXMigrate
   
   public function __construct() {
     $this->pdo = WXActiveRecord::getDefaultPDO();
-    $this->check_schema();
+    if(!$this->check_schema()) {
+      $this->create_schema();
+    }
   }
   
   public function check_schema() {
-    $query = $this->pdo->exec("show tables");
-    $tables = $query->fetchAll();
+    $sth = $this->pdo->query("show tables");
+    while($table = $sth->fetch()) {
+      if($table[0] == "migration_info") {
+        return true;
+      }
+      return false;
+    }
   }
   
+  protected function create_schema() {
+    $this->pdo->query("CREATE TABLE `migration_info` (`version` INT(7) unsigned NOT NULL default '0')");
+    $this->pdo->query("INSERT INTO `migration_info` (`version`) VALUES (0)");    
+  }
+  
+  protected function get_version() {
+    
+  }
 }
 
 
