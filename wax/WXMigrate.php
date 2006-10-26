735a4e36c98c351adc73b143213b03492e620de0
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-10-26 13:15:30 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@429 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXMigrate.php
+++ wax/WXMigrate.php
@@ -93,7 +93,7 @@ class WXMigrate
   }
   
   protected function create_migration_array($directory) {   
-    $migrations=scandir($directory);
+    $migrations=File::scandir_recursive($directory);
     foreach($migrations as $migration) {
       $version_number_of_file = $this->get_version_from_file($migration);
       $class_name = $this->get_class_from_file($migration);
