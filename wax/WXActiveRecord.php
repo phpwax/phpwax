34f1f6a5be5e1ed388c3f8facf2087d7b7021293
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-11-01 15:51:56 +0000

    debugging
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@717 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXActiveRecord.php
+++ wax/WXActiveRecord.php
@@ -232,7 +232,7 @@ class WXActiveRecord extends WXValidations implements Iterator
 		try {
 		  $row_list = $this->query($sql, "all");
 	  } catch(PDOException $e) {
-	    print_r($e); exit;
+	    echo "<pre>"; print_r($e); echo "</pre>"; exit;
       throw new WXActiveRecordException( $e->getMessage(), "Error Preparing Database Query" );
     }
 		$item_list = array();
