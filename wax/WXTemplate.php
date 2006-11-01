ee155de5811586d198b671be4cfdded6d95e6459
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-11-01 13:56:37 +0000

    debugging
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@680 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXTemplate.php
+++ wax/WXTemplate.php
@@ -23,7 +23,8 @@ class WXTemplate
 	}
 	
 	public function parse( $pFile ) {
-	  $raw_view = substr(strrchr($pFile, "/"),1);	  
+	  $raw_view = substr(strrchr($pFile, "/"),1);
+	  echo $this->shared_dir.$raw_view; exit;
 		$this->preserve_buffer ? $buffer = ob_get_clean() : ob_clean();
 		ob_start();
 		if(is_readable(VIEW_DIR.$pFile)) {
