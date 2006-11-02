359a7d2776c8f602f249cfa5cba33f98b0b091dd
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-11-02 11:08:13 +0000

    debugging
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@771 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXTemplate.php
+++ wax/WXTemplate.php
@@ -24,7 +24,7 @@ class WXTemplate
 	
 	public function parse( $pFile ) {
 	  $raw_view = substr(strrchr($pFile, "/"),1);
-	  echo $pFile; exit;
+	  echo $this->plugin_view_path; exit;
 		$this->preserve_buffer ? $buffer = ob_get_clean() : ob_clean();
 		ob_start();
 		if(is_readable(VIEW_DIR.$pFile)) {
