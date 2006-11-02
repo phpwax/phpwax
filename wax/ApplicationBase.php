315e61984f56269408814fd6f73c6a4a3a50b871
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-11-02 09:20:30 +0000

    debugging
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@753 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/ApplicationBase.php
+++ wax/ApplicationBase.php
@@ -155,7 +155,7 @@ class ApplicationBase
    *  @return void
    */	
 	private function create_page($cnt) {
-	  echo rtrim($this->controller, "Controller"); exit;
+	  echo str_replace("Controller", "", $this->controller); exit;
   	$write_to_cache = false;
    	$cache_file   = $this->controller . "_" . $cnt->action;
   	$page_output = false;
