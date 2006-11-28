b2f0dca9d02dd5e12a7d3cffbea5c063b0c4a59a
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-11-28 15:42:45 +0000

    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@1147 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXMigrate.php
+++ wax/WXMigrate.php
@@ -131,7 +131,8 @@ class WXMigrate
     $migrations=File::scandir_recursive($directory);
     if(count($migrations)<1) return "No migrations in supplied directory";
     foreach($migrations as $migration) {
-      if(strpos($migration, ".php")) include_once($migration);
+      if(!strpos($migration, ".php")) return "Only directories of PHP files are allowed"; 
+      include_once($migration);
       $instance = new $this->get_class_from_file($migration);
       $instance->up();
     }
