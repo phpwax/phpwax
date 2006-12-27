a349a0d73a11e9279a4879eaf6bea9ab021ca72b
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2006-12-27 19:09:31 +0000

    added debug statement to update_Attributes
    git-svn-id: svn://php-wax.com/svn/wxframework/trunk@1507 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXActiveRecord.php
+++ wax/WXActiveRecord.php
@@ -630,6 +630,7 @@ class WXActiveRecord extends WXValidations implements Iterator
 	public function handle_post($attributes=null) {
 	  if($this->is_posted()) {
 	    if(!$attributes) $attributes = $_POST[WXInflections::underscore(get_class($this))];
+	    error_log($attributes);
 	    return $this->update_attributes($attributes);
 	  }
 	  return false;
