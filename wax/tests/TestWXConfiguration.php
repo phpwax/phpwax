172cc06ec968b511b023f33915bae0ddff3add62
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-05 11:01:24 +0000

    new tests
    git-svn-id: svn://php-wax.com/svn/main/trunk@1571 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ wax/tests/TestWXConfiguration.php
@@ -0,0 +1,32 @@
+<?php
+
+class TestWXActiveRecord extends WXTestCase 
+{
+    public function setUp() {
+      
+    }
+    
+    public function tearDown() {
+      
+    }
+
+  	public function test_replace_yaml() {
+  	  
+  	}
+
+  	public function test_set() {
+  	  
+  	}
+
+    public function test_get($value) { 
+  	  $config = WXConfiguration::get('db');
+  	  $this->assertTrue(is_array($config));
+  	}
+
+  	public function test_set_environment($env) {
+  	  
+  	}
+
+    
+}
+?>
