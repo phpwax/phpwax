07f112d7e46b20205839b21e088e19cc0e932d66
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-10 11:54:17 +0000

    rearranged scriptaculous load order
    git-svn-id: svn://php-wax.com/svn/main/trunk@1597 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/helpers/AssetTagHelper.php
+++ wax/helpers/AssetTagHelper.php
@@ -23,7 +23,7 @@ class AssetTagHelper extends WXHelpers {
      *  @uses javascript_default_sources
      */
     function __construct() {
-        $this->javascript_default_sources =	array('prototype', 'effects', 'controls', 'dragdrop', 'builder');    
+        $this->javascript_default_sources =	array('prototype', 'builder','effects', 'dragdrop', 'controls');    
     }
     
     /**
