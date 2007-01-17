5c31040483040f56973ac233b2f0dfb2da0acab7
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-17 14:22:02 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/main/trunk@1686 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/AutoLoader.php
+++ wax/AutoLoader.php
@@ -108,7 +108,7 @@ class AutoLoader
 	static public function detect_environments() {
 	  foreach(WXConfiguration::get("environments") as $env=>$range) {
 	    echo $env.":".$range.":";
-	    if(preg_match($range, $_SERVER['ADDR'])) {
+	    if(preg_match("/".$range."/", $_SERVER['ADDR'])) {
 	      //define('ENV', $env);
 	      echo "MATCHED"."\n";
 	    }
