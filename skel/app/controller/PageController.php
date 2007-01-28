f030d2eefdab05a494b94f9c92ef3e3b14a1e6f3
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-01-28 11:40:26 +0000

    Made the default install much more user friendly. Pre-setup a default controller and view. Worried we're making it all too easy.
    git-svn-id: svn://php-wax.com/svn/main/trunk@2084 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ skel/app/controller/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Page Controller
+ *
+ * This is a default controller installed by PHP-WAX
+ * Feel free to use this one, or just create your own using 'script/new_controller my_name'
+ *
+ * All you need to do in this controller is make one public method for each url.
+ * Then make html tempalates in the 'view/page' directory.
+ * The default 'index' is setup already.
+ **/
+
+class PageController extends ApplicationController {
+  
+  public function index() {
+    
+  }
+  
+}
