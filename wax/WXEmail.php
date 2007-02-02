6a75749ef92479822ba3d8cd0ddcb60b5a8e14c6
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-02-02 14:59:40 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/main/trunk@2157 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXEmail.php
+++ wax/WXEmail.php
@@ -1018,7 +1018,7 @@ class WXEmail
       if(substr($name, 0,4)=="send") {
         $action = substr($name, 5);
         $this->get_templates($action);
-        call_user_func($action, $args);
+        call_user_func(array($this, $action), $args);
         $this->send();
       }
     }
