efd48214b157d4870043545c3a3869b02cf6bd33
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-02-02 15:06:31 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/main/trunk@2159 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXEmail.php
+++ wax/WXEmail.php
@@ -1002,7 +1002,6 @@ class WXEmail
       $view = WXInflections::underscore(get_class($this));
       $html = VIEW_DIR.$view."/".$action.".html";
       $txt =  VIEW_DIR.$view."/".$action.".txt";
-      echo $html.": ".$txt; exit;  
       if(is_readable($html && is_readable($txt))) {
         $this->is_html = true;
         $this->body=WXControllerBase::view_to_string($html, $this);
