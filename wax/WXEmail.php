08d86826b5c5e2cda16d6087aefca696c707e842
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-02-02 16:44:51 +0000

    debugging
    git-svn-id: svn://php-wax.com/svn/main/trunk@2171 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXEmail.php
+++ wax/WXEmail.php
@@ -1016,7 +1016,6 @@ class WXEmail
     }
     
     public function __call($name, $args) {
-      print_r($args); exit;
       if(substr($name, 0,4)=="send") {
         $action = substr($name, 5);
         $this->get_templates($action);
