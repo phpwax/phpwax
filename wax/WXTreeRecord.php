2dae0c2cfd5584126aa6f7687422f7dec05d214c
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-02-05 11:26:40 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/main/trunk@2199 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXTreeRecord.php
+++ wax/WXTreeRecord.php
@@ -78,6 +78,7 @@ class WXTreeRecord extends WXActiveRecord implements RecursiveIterator {
 	public function find_roots($params = array()) {
 	  if(array_key_exists("conditions", $params)) $params["conditions"]=$params["conditions"]." AND "."{$this->rel_column} = 0";
 		else $params = array_merge($params, array("conditions"=>"{$this->rel_column} = 0"));
+		print_r($params); exit;
 		return $this->find_all($params);
 	}
 	
