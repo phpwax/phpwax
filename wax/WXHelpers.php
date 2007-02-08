335a405315dd727911267a8af2bd785d6186ec62
charles <charles@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-02-08 11:40:37 +0000

    new pagination features
    git-svn-id: svn://php-wax.com/svn/main/trunk@2268 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXHelpers.php
+++ wax/WXHelpers.php
@@ -254,7 +254,13 @@ class WXHelpers {
 		return false;
 	}
 	
-	public function pagination_links($model){
+	public function pagination_links($model, $action="", $controller=""){
+		$path = array();
+		if($controller)
+			$path['controller'] = $controller;
+		if($action)
+			$path['action'] = $action;
+			
 		//if model has a total and less the 10 pages just show a list of all pages
 		if($model->paginate_total && ($model->paginate_total < 10) ){
 			//find max pages
@@ -266,7 +272,8 @@ class WXHelpers {
 				} else {
 					$output .= "<li>";
 				}
-				$output .= link_to($page_number, array("id"=>$page_number) ) . "</li>";
+				$path['id'] = $page_number;
+				$output .= link_to($page_number, $path ) . "</li>";
 			}
 			$output .= "</ul>";
 			return $output;
