0b47dec9ed01c3e601949502cb9a93bf0bda1bce
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-02-09 12:26:41 +0000

    bug fix
    git-svn-id: svn://php-wax.com/svn/main/trunk@2283 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXHelpers.php
+++ wax/WXHelpers.php
@@ -265,7 +265,7 @@ class WXHelpers {
 			//find max pages
 			$max_pages = round($model->paginate_total / $model->paginate_limit);
 			if($max_pages < 1) return false;
-			$output ="<ul id='pagination'><li class='page'>Pages</li>";
+			$output ="<ul id='pagination'>";
 			for($page_number=1; $page_number <= $max_pages; $page_number++){
 				if($page_number == $model->paginate_page){
 					$output .= "<li class='active-page'>";
