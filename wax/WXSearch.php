bb9f94d376c5b917acff5ac11aeaeb24b0b9b03f
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-13 17:40:56 +0000

    new search class
    git-svn-id: svn://php-wax.com/svn/main/trunk@2486 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ wax/WXSearch.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * @package PHP-WAX
+ * @author Ross Riley
+ **/
+class WXSearch {
+
+	static public $search_array=array();
+	public $search_phrase=false;
+	
+	
+	public function __construct($search_phrase) {
+	  $this->search_phrase = $search_phrase;
+	}
+	
+	static public function register_search($key, $table, $field) {
+	  self::$search_array[]=array("key"=>$key, "table"=>$table, "field"=>$field);
+	}
+	
+	static public function unregister_search($key) {
+	  for($i=0;$i<=count(self::$search_array); $i++) {
+	    if(self::$search_array[$i]['key'] == $key) unset(self::$search_array[$i]);
+	  } 
+	}
+	
+	public function get_results() {
+	  $setups=array();
+	  $tables=array();
+	  $fields=array();
+	  foreach(self::$search_array as $search) {
+	    $setups[]= "ALTER TABLE ".$search['table']." ADD FULLTEXT (".$search['field'].");";
+	    $query = "SELECT *, MATCH(".$search('field').") AGAINST('".$this->search_phrase."')
+	      FROM ".$search['table']."WHERE MATCH(".$search('field').") AGAINST('".$this->search_phrase."')";
+	    $table = new $search['table'];
+	    $results[$search['key']]=$table->find_by_sql($query);
+	  }
+	  return $results;
+	}
+
+}
+?>
