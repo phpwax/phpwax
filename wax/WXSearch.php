3faa263f59effd774011c52d6767ff00a46312f6
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-14 09:13:50 +0000

    new search class
    git-svn-id: svn://php-wax.com/svn/main/trunk@2487 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXSearch.php
+++ wax/WXSearch.php
@@ -32,7 +32,8 @@ class WXSearch {
 	    $setups[]= "ALTER TABLE ".$search['table']." ADD FULLTEXT (".$search['field'].");";
 	    $query = "SELECT *, MATCH(".$search('field').") AGAINST('".$this->search_phrase."')
 	      FROM ".$search['table']."WHERE MATCH(".$search('field').") AGAINST('".$this->search_phrase."')";
-	    $table = new $search['table'];
+	    $model = $search['table']
+	    $table = new $model;
 	    $results[$search['key']]=$table->find_by_sql($query);
 	  }
 	  return $results;
