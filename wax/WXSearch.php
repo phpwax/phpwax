982bdb2d4947cc5d9072e392530fe4f2bd6f6db5
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-14 09:48:07 +0000

    search setup
    git-svn-id: svn://php-wax.com/svn/main/trunk@2499 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXSearch.php
+++ wax/WXSearch.php
@@ -32,9 +32,7 @@ class WXSearch {
 	     FROM ".$search['table']." WHERE MATCH(".$search['field'].") AGAINST('".$this->search_phrase."')";
 	    $model = WXInflections::camelize($search['table'], true);
 	    $table = new $model;
-	    $db = WXActiveRecord::getDefaultPDO();
-	    print_r($db); exit;
-	    $db->query($setup);
+	    WXActiveRecord::getDefaultPDO()->query($setup);
 	    $results[$search['key']]=$table->find_by_sql($query);
 	  }
 	  return $results;
