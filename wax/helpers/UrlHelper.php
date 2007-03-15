20325479fe35767605ce83315a3755ef7542787a
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-15 12:10:17 +0000

    fix to bad url creation
    git-svn-id: svn://php-wax.com/svn/main/trunk@2506 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/helpers/UrlHelper.php
+++ wax/helpers/UrlHelper.php
@@ -184,11 +184,9 @@ class UrlHelper extends WXHelpers {
 
 		if(!count($extra_params)) {
 		  if(array_pop(array_values($url))=="index") array_pop($url);
-    	return $url_base.implode("/", $url)."/";
+    	return str_replace("//","/",$url_base.implode("/", $url)."/");
 		} 
-    return $url_base . implode("/", $url)."/"
-          . (count($extra_params)
-             ? "?".http_build_query($extra_params) : null);
+    return $url_base . str_replace("//", "/", implode("/", $url)."/"). "?".http_build_query($extra_params));
     }   
 
 }
