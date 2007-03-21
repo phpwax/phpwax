f8593f9406fdad011b77192dda802679ef00a229
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-21 16:27:16 +0000

    yui debug
    git-svn-id: svn://php-wax.com/svn/main/trunk@2554 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/helpers/YUIHelper.php
+++ wax/helpers/YUIHelper.php
@@ -25,9 +25,9 @@ class YUIHelper extends JavascriptHelper{
   }
   
   public function yui_container_files() {
-    $css = array("http://yui.yahooapis.com/2.2.0/build/container/assets/container.css");
     $js = array("http://yui.yahooapis.com/2.2.0/build/container/container-min.js");
-    return $this->yui_csslink($css).$this->yui_jsinclude($js);
+    $css = array("http://yui.yahooapis.com/2.2.0/build/container/assets/container.css");
+    return $this->yui_jsinclude($js).$this->yui_csslink($css);
   }
   
   public function yui_container($element, $options=array()) {
