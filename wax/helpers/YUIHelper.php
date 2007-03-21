de15465f6cbad18f81ffc2fdd2faff6f13b6ad5c
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-21 14:32:08 +0000

    yui debug
    git-svn-id: svn://php-wax.com/svn/main/trunk@2542 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/helpers/YUIHelper.php
+++ wax/helpers/YUIHelper.php
@@ -35,13 +35,12 @@ class YUIHelper extends JavascriptHelper{
       "underlay"=>"'shadow'", "fixedcenter"=>"true");
     $options = array_merge($default_options, $options);
     $javascript = 'YAHOO.namespace("cms.container");';
-    $javascript .= 'function init_'.$element.'() {';
+    $javascript .= 'function init_'.$element.'() { alert("working");';
     $javascript .= 'YAHOO.cms.container.'.$element.' = new YAHOO.widget.Panel("'.$element.'",'.$this->options_for_javascript($options).');'; 
     $javascript .= 'YAHOO.cms.container.'.$element.'.render(); }'."\n";
     $javascript .= 'YAHOO.util.Event.addListener(window, "load", init_'.$element.');';
     return $this->javascript_tag($javascript);
   }
-  
   
   protected function yui_csslink($sources, $options=array()) {
     foreach($sources as $source) {
