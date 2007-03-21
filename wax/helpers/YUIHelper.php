b920c3dffed40c48d438c628dc1afb5d96cb4649
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-03-21 14:34:40 +0000

    yui debug
    git-svn-id: svn://php-wax.com/svn/main/trunk@2544 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/helpers/YUIHelper.php
+++ wax/helpers/YUIHelper.php
@@ -37,7 +37,7 @@ class YUIHelper extends JavascriptHelper{
     $javascript = 'YAHOO.namespace("cms.container");';
     $javascript .= 'function init_'.$element.'() {';
     $javascript .= 'YAHOO.cms.container.'.$element.' = new YAHOO.widget.Panel("'.$element.'",'.$this->options_for_javascript($options).');'; 
-    $javascript .= 'alert(YAHOO.cms.container.'.$element.');'
+    $javascript .= 'alert(YAHOO.cms.container.'.$element.');';
     $javascript .= 'YAHOO.cms.container.'.$element.'.render(); }'."\n";
     $javascript .= 'YAHOO.util.Event.addListener(window, "load", init_'.$element.');';
     return $this->javascript_tag($javascript);
