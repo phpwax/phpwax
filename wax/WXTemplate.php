729d1044bb925e13cd1036da53d461b228d539eb
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-04-19 12:18:26 +0000

    debug line
    git-svn-id: svn://php-wax.com/svn/main/trunk@2865 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXTemplate.php
+++ wax/WXTemplate.php
@@ -26,7 +26,7 @@ class WXTemplate
 	    case "js": $type="javascript";
 	    default: $type=$suffix;
 	  }
-	  header("Content-Type: text/$type; charset=utf-8");
+	  //header("Content-Type: text/$type; charset=utf-8");
 	  foreach($this->template_paths as $path) {
 	    if(is_readable($path.".".$suffix)) {
 				$view_file = $path.".".$suffix;
