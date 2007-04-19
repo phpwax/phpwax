105f5bc41f735c29060ba5f8ebb68d449d438b3c
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-04-19 10:55:22 +0000

    debug line
    git-svn-id: svn://php-wax.com/svn/main/trunk@2857 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXControllerBase.php
+++ wax/WXControllerBase.php
@@ -207,12 +207,11 @@ abstract class WXControllerBase
 	  if(!$this->action = $this->route_array[0]) {
 	    $this->action = "index";
 	  }
-	  die($this->action);
 	  if(strpos($this->action, ".")) {
 	    $this->action = substr($this->action,0,strpos($this->action,"."));
 	    $this->use_format = substr(strstr($this->action, "."), 1);
-	    error_log($this->action);
-	    error_log($this->use_format); exit;
+	    die($this->action.":".$this->use_format);
+	    exit;
 	  }
 	  array_shift($this->route_array);
 	  $this->controller_global();
