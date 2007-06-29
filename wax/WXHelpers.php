011a1a7e48f4c5f29a8064dd33aba0f8688a57be
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-06-29 14:31:49 +0000

    bug fix to pagination
    git-svn-id: svn://php-wax.com/svn/main/trunk@3217 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/WXHelpers.php
+++ wax/WXHelpers.php
@@ -276,6 +276,7 @@ class WXHelpers {
 	}
 	
 	public function paginate_links(WXPaginatedRecordset $recordset, $window = "1", $prev_content="&laquo;", $next_content="&raquo;", $param="page") {
+    if($recordset->total_pages <=1) return false;
     $content = "";
     $page = 1; $links = array();
     if($prev_content && !$recordset->is_current($page)) $links[]=link_to($prev_content, array($param=>$recordset->previous_page()));
