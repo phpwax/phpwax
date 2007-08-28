f58fd27927382924a5a1c72a0790285209325d19
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2007-08-28 18:14:34 +0000

    bug fix to layered plugin support
    git-svn-id: svn://php-wax.com/svn/main/trunk@3839 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/AutoLoader.php
+++ wax/AutoLoader.php
@@ -104,9 +104,11 @@ class AutoLoader
   	    if(is_dir(PLUGIN_DIR.$plugin)) {
   	      $type = array_shift($asset_paths);
   	      $path = PLUGIN_DIR.$plugin."/resources/public/".$type."/".implode("/", $asset_paths);
-  	      if($type=="images") File::display_image($path);
-  	      if($type=="javascripts") File::display_asset($path, "text/javascript");
-  	      if($type=="stylesheets") File::display_asset($path, "text/css");
+  	      if(is_readable($path)) {
+  	        if($type=="images") File::display_image($path);
+  	        if($type=="javascripts") File::display_asset($path, "text/javascript");
+  	        if($type=="stylesheets") File::display_asset($path, "text/css");
+	        }
   	    }
   	  }
 	  }
