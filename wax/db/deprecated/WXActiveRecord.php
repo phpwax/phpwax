77300f558e0e4e6ed5103ead2c069d0ec21cb17d
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-02-14 15:43:58 +0000

    Security bug fix
    git-svn-id: svn://php-wax.com/svn/main/trunk@4481 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/deprecated/WXActiveRecord.php
+++ wax/db/deprecated/WXActiveRecord.php
@@ -739,6 +739,9 @@ class WXActiveRecord extends WXValidations implements Iterator
 		  $what[$key]=rtrim(ltrim($val, "_"), "_");
 		}
     if( $args ) {
+      for($i=1; $i<=count($args); $i++) {
+        $args[$i]=$this->pdo->quote($args[$i]);
+      }
 			if(count($what)==2) { 
 				$conds=$what[0]."='".$args[0]."' AND ".$what[1]."='".$args[1]."'";
 			}else{
