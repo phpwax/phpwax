9ccc7a43d152156cc6509aeced06c2e6ad95ac64
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-03-25 15:24:59 +0000

    Work on implementing model fields
    git-svn-id: svn://php-wax.com/svn/main/trunk@4761 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ wax/db/WaxModelFields.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * WaxModelFields class
+ *
+ * @package default
+ * @author /bin/bash: niutil: command not found
+ **/
+class WaxModelFields {
+
+  public function __construct($field_type, $options = array()) {
+    
+  }
+  
+  public function char_field() {
+    
+  }
+  
+  public function text_field() {
+    
+  }
+  
+  public function 
+
+
+
+
+
+
+} // END class 
+
