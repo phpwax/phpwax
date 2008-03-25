6b6ad024db1ab755970d492a79793f3a1d6b085e
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-03-25 20:34:20 +0000

    Work on ModelFields and Validation
    git-svn-id: svn://php-wax.com/svn/main/trunk@4767 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/WaxModelField.php
+++ wax/db/WaxModelField.php
@@ -3,44 +3,37 @@
 /**
  * WaxModelFields class
  *
- * @package default
- * @author /bin/bash: niutil: command not found
+ * @package PHP-Wax
  **/
 class WaxModelField {
   
   public $null = false;
+  public $default = false;
   public $maxlength = false;
   public $minlength = false;
   public $choices = false;
   public $blank = true;
   public $label = false;
   public $help_text = false;
+  public $format = false;
+  public $validations = array();
+  public $errors = array();
+  
   
 
   public function __construct($field_type, $options = array()) {
     
   }
   
-  public function char_field() {
+  public function validate($model) {
     
   }
   
-  public function text_field() {
+  public function form_field() {
     
   }
   
-  public function date_field() {
-    
-  }
   
-  public function datetime_field() {
-    
-  }
-
-  public function email_field() {
-    
-  }
-
 
 
 } // END class 
