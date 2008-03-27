446ab608c9f55d150be619636f7b0cc0c8e62a9b
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-03-27 18:12:06 +0000

    New EmailField type
    git-svn-id: svn://php-wax.com/svn/main/trunk@4772 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ wax/db/fields/EmailField.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * EmailField class
+ *
+ * @package PHP-Wax
+ **/
+class EmailField extends WaxModelField {
+  
+  public $null = false;
+  public $default = false;
+  public $maxlength = "100";
+  
+  public function setup() {
+    
+  }
+
+  public function validate() {
+    $this->valid_length();
+    $this->valid_format("email", '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i');
+  }
+
+
+} 
