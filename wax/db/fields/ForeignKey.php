6cbb26992d05751aa2b32efe7f612315a8029409
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-01 10:44:28 +0000

    Work on tests for new model
    git-svn-id: svn://php-wax.com/svn/main/trunk@4948 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/fields/ForeignKey.php
+++ wax/db/fields/ForeignKey.php
@@ -18,7 +18,7 @@ class ForeignKey extends WaxModelField {
   public function setup() {
     if(!$this->table) $this->table = $this->field;
     if(!$this->col_name) $this->col_name = $this->table."_id";
-    if(!$this->model_name) $this->model_name = WaxInflections::camelize($this->table);
+    if(!$this->model_name) $this->model_name = Inflections::camelize($this->table);
   }
 
   public function validate() {
