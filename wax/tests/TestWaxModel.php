429ccd286a2db798dd3c0beea8af3d9b881e814d
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-01 10:41:17 +0000

    Work on tests for new model
    git-svn-id: svn://php-wax.com/svn/main/trunk@4875 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/tests/TestWaxModel.php
+++ wax/tests/TestWaxModel.php
@@ -1,15 +1,20 @@
 <?php
-class TestModel extends WaxModel {
-  
+class ExampleModel extends WaxModel {
+  public function setup() {
+    $this->define("username", "CharField", array("maxlength"=>40));
+    $this->define("password", "CharField", array("blank"=>false, "maxlength"=>15));
+    $this->define("email", "EmailField", array("blank"=>false));
+  }
 }
 
 class TestWaxModel extends WXTestCase {
     public function setUp() {
-      $this->model = new TestModel();
+      $this->model = new ExampleModel();
+      $this->model->syncdb();
     }
     
     public function tearDown() {
-
+      
     }
     
     
@@ -20,6 +25,12 @@ class TestWaxModel extends WXTestCase {
         "user3" => array("username"=>"test1", "password"=>"password", "email"=>"test3@test.com")
       );
       return $fixtures[$type];
+    }
+    
+    public function test_create() {
+      $this->model->create($this->get_fixture("user1"));
+      $res = $this->model->first();
+      $this->assertIsA($res, "WaxModel");
     }
     
     
