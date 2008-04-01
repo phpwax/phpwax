cb973a7de5b1ba5ae378642c32bcb27b448992c8
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-01 10:42:27 +0000

    Work on tests for new model
    git-svn-id: svn://php-wax.com/svn/main/trunk@4901 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/WaxModel.php
+++ wax/db/WaxModel.php
@@ -69,7 +69,7 @@ class WaxModel {
  	  foreach($this->columns as $column=>$setup) {
  	    $field = new $setup[0]($column, $this, $setup[1]);
  	    $field->validate();
- 	    if(count($field->errors) >0) {
+ 	    if(count($field->errors) >1) {
  	      error_log($column." has errors");
  	      $this->errors[$column] = $field->errors;
       }
