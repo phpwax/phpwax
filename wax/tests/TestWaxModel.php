703bfc33481d37ff26b59adfcf9bf4e4d9179204
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-01 10:44:06 +0000

    Work on tests for new model
    git-svn-id: svn://php-wax.com/svn/main/trunk@4940 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/tests/TestWaxModel.php
+++ wax/tests/TestWaxModel.php
@@ -66,7 +66,7 @@ class TestWaxModel extends WXTestCase {
     public function test_multiple_filters() {
       $this->model->create($this->get_fixture("user2"))->update_attributes(array("username"=>"altered"));
       $this->model->create($this->get_fixture("user3"));
-      $res = $this->model->filter("username = 'altered'")->filter(array("username"=>"test2"))->all();
+      $res = $this->model->filter(array("password"=>"password"))->all();
       $this->assertEqual($res->count(), "2");
       $this->model->delete();
     }
