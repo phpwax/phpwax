9bae6ad3a1ac6860ca566ce69dd50e1b994cdc1d
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-02 09:22:28 +0000

    Work on tests for new model
    git-svn-id: svn://php-wax.com/svn/main/trunk@5055 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/tests/TestWaxModel.php
+++ wax/tests/TestWaxModel.php
@@ -32,45 +32,45 @@ class TestWaxModel extends WXTestCase {
       $this->assertEqual($res->username, "test1");
     }
     
-    // public function test_all() {
-    //      $res = $this->model->all();
-    //      $this->assertIsA($res, "WaxRecordset");
-    //    }
-    //    
-    //    public function test_first() {
-    //      $res = $this->model->first();
-    //      $this->assertIsA($res, "WaxModel");
-    //      $this->assertEqual($res->username, "test1");
-    //    }
-    //    
-    //    public function test_delete() {
-    //      $res = $this->model->filter(array("username"=>"test1"))->all()->delete();
-    //      $res = $this->model->filter(array("username"=>"test1"))->first();
-    //      $this->assertFalse($res->count(), "0");
-    //    }
-    //    
-    //    public function test_multiple_delete() {
-    //      $this->model->create($this->get_fixture("user1"));
-    //      $this->model->create($this->get_fixture("user2"));
-    //      $this->assertEqual($this->model->all()->count(), "2");
-    //      $res = $this->model->all()->delete();
-    //      $this->assertEqual($this->model->all()->count(), "0");
-    //    }
-    //    
-    //    public function test_update() {
-    //      $this->model->create($this->get_fixture("user1"))->update_attributes(array("username"=>"altered"));
-    //      $res = $this->model->filter(array("username"=>"altered"))->all();
-    //      $this->assertEqual($res->count(), "1");
-    //      $this->model->clear()->delete();
-    //    }
-    //    
-    //    public function test_multiple_filters() {
-    //      $this->model->create($this->get_fixture("user2"))->update_attributes(array("username"=>"altered"));
-    //      $this->model->create($this->get_fixture("user3"));
-    //      $res = $this->model->filter(array("password"=>"password"))->all()->filter("username !='altered'")->all();
-    //      $this->assertEqual($res->count(), 1);
-    //      $this->model->clear()->delete();
-    //    }
+    public function test_all() {
+      $res = $this->model->all();
+      $this->assertIsA($res, "WaxRecordset");
+    }
+    
+    public function test_first() {
+      $res = $this->model->first();
+      $this->assertIsA($res, "WaxModel");
+      $this->assertEqual($res->username, "test1");
+    }
+    
+    public function test_delete() {
+      $res = $this->model->filter(array("username"=>"test1"))->all()->delete();
+      $res = $this->model->filter(array("username"=>"test1"))->first();
+      $this->assertFalse($res->count(), "0");
+    }
+    
+    public function test_multiple_delete() {
+      $this->model->create($this->get_fixture("user1"));
+      $this->model->create($this->get_fixture("user2"));
+      $this->assertEqual($this->model->all()->count(), "2");
+      $res = $this->model->all()->delete();
+      $this->assertEqual($this->model->all()->count(), "0");
+    }
+    
+    public function test_update() {
+      $this->model->create($this->get_fixture("user1"))->update_attributes(array("username"=>"altered"));
+      $res = $this->model->filter(array("username"=>"altered"))->all();
+      $this->assertEqual($res->count(), "1");
+      $this->model->clear()->delete();
+    }
+    
+    public function test_multiple_filters() {
+      $this->model->create($this->get_fixture("user2"))->update_attributes(array("username"=>"altered"));
+      $this->model->create($this->get_fixture("user3"));
+      $res = $this->model->filter(array("password"=>"password"))->all()->filter("username !='altered'")->all();
+      $this->assertEqual($res->count(), 1);
+      $this->model->clear()->delete();
+    }
     
 }
 
