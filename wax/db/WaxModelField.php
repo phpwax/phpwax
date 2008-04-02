2bcdf24e3e9e1cad8a95ec74bc490b010f63ca68
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-02 09:22:02 +0000

    Work on tests for new model
    git-svn-id: svn://php-wax.com/svn/main/trunk@5045 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/WaxModelField.php
+++ wax/db/WaxModelField.php
@@ -41,9 +41,8 @@ class WaxModelField {
     $this->model = $model;
     foreach($options as $option=>$val) $this->{$option} = $val;
     if(!$this->field) $this->field = $column;
-    
-    $this->setup();
     if(!$this->col_name) $this->col_name = $this->field;
+    $this->setup();
   }
   
   public function get() {
