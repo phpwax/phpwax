c1b34b5a76c07f6030b9909d6f7447f2e438e844
sheldon <sheldon@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-02 09:33:11 +0000

    boolean field added
    git-svn-id: svn://php-wax.com/svn/main/trunk@5086 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

---
+++ wax/db/fields/BooleanField.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * BooleanField class
+ *
+ * @package PHP-Wax
+ **/
+class BooleanField extends WaxModelField {
+  
+  public $null = false;
+  public $default = 0;
+  public $maxlength = false;
+  public $choices = array(0 => "false", 1 => "true");
+  
+  public function setup() {
+    
+  }
+
+  public function validate() {
+    $this->valid_length();
+ 	  $this->valid_required();
+    $this->valid_format("number", "/^[0-1]$/");
+  }
+
+} 
