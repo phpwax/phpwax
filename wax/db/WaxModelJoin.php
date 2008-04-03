cd596730416f1621f0eee41378602deac22ba59f
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-03 14:45:37 +0000

    Created ManyToMany functionality
    git-svn-id: svn://php-wax.com/svn/main/trunk@5138 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/WaxModelJoin.php
+++ wax/db/WaxModelJoin.php
@@ -4,6 +4,12 @@ class WaxModelJoin extends WaxModel {
   public $left_field = false;
   public $right_field = false;
   
+  public function __construct() {
+    $this->db = self::$adapter;
+ 		$this->define($this->primary_key, $this->primary_type, $this->primary_options);
+ 		$this->setup();
+  }
+  
   public function init(WaxModel $left, WaxModel $right) {
     $this->table = $left->table."_".$right->table;
     $this->define($left->table."_".$left->primary_key, "IntegerField");
