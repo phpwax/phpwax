2a9e7c15a6ed8d9a9a884bd08f9f75b1be5bc1a2
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-03 14:43:53 +0000

    Created ManyToMany functionality
    git-svn-id: svn://php-wax.com/svn/main/trunk@5098 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/tests/TestWaxModelField.php
+++ wax/tests/TestWaxModelField.php
@@ -70,6 +70,8 @@ class TestWaxModelField extends WXTestCase {
       $prop1 = $props->create(array("name"=>"Property 1"));
       $prop2 = $props->create(array("name"=>"Property 2"));
       $model->properties = $props->all();
+      $this->assertIsA($model->properties, "WaxRecordset");
+      $this->assertEqual($model->properties->count(), "2");
     }
 
     
