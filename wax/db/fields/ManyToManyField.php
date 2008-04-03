58bef8e030a076a99fd54f44a5c3b7dd71d9b2db
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-03 14:49:35 +0000

    Created ManyToMany functionality
    git-svn-id: svn://php-wax.com/svn/main/trunk@5226 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/fields/ManyToManyField.php
+++ wax/db/fields/ManyToManyField.php
@@ -38,7 +38,7 @@ class ManyToManyField extends WaxModelField {
   public function get() {
     $vals = $this->join_model->all();
     $links = new $this->hasmany_model;
-    foreach($vals as $val) $links->filter(array($links->primary_key=> $val->right_field));
+    foreach($vals as $val) $links->filter(array($links->primary_key=> $val->{$val->right_field}));
     return new WaxModelAssociation($links);
   }
   
