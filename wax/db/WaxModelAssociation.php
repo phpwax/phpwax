10b064db24bbc9745d588618cac5f0f1530bf0d6
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-04 10:33:22 +0000

    Final touches to ManyToMany model tests : fingers crossed
    git-svn-id: svn://php-wax.com/svn/main/trunk@5264 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/WaxModelAssociation.php
+++ wax/db/WaxModelAssociation.php
@@ -11,26 +11,26 @@ class WaxModelAssociation extends WaxRecordset {
   
   public $field;
   public $target_model;
-  public $join_model;
+  public $model;
 
   public function __construct($target, $join_model) {
     parent::__construct($target, $target->all()->rowset);
     $this->field = $field;
     $this->target_model = $target;
-    $this->join_model = $join_model;
+    $this->model = $join_model;
   } 
   
   public function unlink($model) {
     if($model instanceof WaxModel) {
       $id = $model->primval;
-      $this->join_model->filter(array($this->target_model->table."_".$this->target_model->primary_key => $id))->delete();
+      $this->model->filter(array($this->target_model->table."_".$this->target_model->primary_key => $id))->delete();
     }
     if($model instanceof WaxRecordset) {
       foreach($model as $obj) {
         $id = $obj->primval;
         $filter[]= $this->target_model->table."_".$this->target_model->primary_key."=".  $id;
       }
-      $this->join_model->filter("(".join(" OR ", $filter).")")->delete();
+      $this->model->filter("(".join(" OR ", $filter).")")->delete();
     }
     return $this->all();
   }
