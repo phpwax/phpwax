5f282b07174116de7f1cdbcd067438d2023f38bb
ross <ross@0e7ba554-dfdc-446b-ac9a-b820edd20ad7>
2008-04-04 10:32:17 +0000

    Created ManyToMany functionality
    git-svn-id: svn://php-wax.com/svn/main/trunk@5239 0e7ba554-dfdc-446b-ac9a-b820edd20ad7

--- wax/db/WaxModelAssociation.php
+++ wax/db/WaxModelAssociation.php
@@ -10,7 +10,7 @@
 class WaxModelAssociation extends WaxRecordset {
 
   public function __construct($model) {
-    print_r($model);
+    print_r($model->all());
     parent::__construct($model, $model->all()->rowset);
   } 
   
