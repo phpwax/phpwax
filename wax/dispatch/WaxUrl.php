0265ecefd0fcf7230acc476b45c9fc6642dfd479
Ross Riley <rossriley@Macintosh.local>
2008-04-05 10:25:27 +0100

    Work on new WaxUrl

--- wax/dispatch/WaxUrl.php
+++ wax/dispatch/WaxUrl.php
@@ -85,7 +85,11 @@ class WaxUrl {
           array_shift($mappings);
         }
       }
-      if($mapped_route) {print_r($mapped_route); return true;}
+      if($mapped_route) {
+        foreach($mapped_route as $k=>$val) {
+          $_GET[$k]=$val;
+        }
+      }
     }
   }
   
