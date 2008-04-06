fa995d57aee6b23ab5eee085e454bddea963eba7
Ross Riley <rossriley@Macintosh.home>
2008-04-06 22:48:57 +0100

    Work on new WaxUrl Cleanup of deprecated files

--- wax/tests/TestWaxUrl.php
+++ wax/tests/TestWaxUrl.php
@@ -10,27 +10,28 @@ class TestWaxUrl extends WXTestCase {
       echo "\n"."-------------"."\n";
     }
     
-    // public function test_basic_map() {
-    //   $_GET["route"]="mycontroller/myaction/myid";
-    //   WaxUrl::perform_mappings();
-    //   $this->assertEqual(WaxUrl::get("controller"), "mycontroller");
-    //   $this->assertEqual(WaxUrl::get("action"), "myaction");
-    //   $this->assertEqual(WaxUrl::get("id"), "myid");
-    // }
-    // 
-    // public function test_default_map() {
-    //   $_GET["route"]="";
-    //   WaxUrl::perform_mappings();
-    //   $this->assertEqual(WaxUrl::get("controller"), "page");
-    // }
-    // 
-    // public function test_pattern_map() {
-    //   $_GET["route"]="blog/tech/5";
-    //   WaxUrl::map("blog/:category/:id", array("controller"=>"blog", "action"=>"show"));
-    //   WaxUrl::perform_mappings();
-    //   $this->assertEqual(WaxUrl::get("controller"), "blog");
-    //   $this->assertEqual(WaxUrl::get("category"), "tech");
-    // }
+    public function test_basic_map() {
+      $_GET["route"]="mycontroller/myaction/myid";
+      WaxUrl::perform_mappings();
+      $this->assertEqual(WaxUrl::get("controller"), "mycontroller");
+      $this->assertEqual(WaxUrl::get("action"), "myaction");
+      $this->assertEqual(WaxUrl::get("id"), "myid");
+    }
+    
+    public function test_default_map() {
+      $_GET["route"]="";
+      WaxUrl::perform_mappings();
+      $this->assertEqual(WaxUrl::get("controller"), "page");
+    }
+    
+    public function test_pattern_map() {
+      $_GET["route"]="blog/tech/5";
+      WaxUrl::map("blog/:category/:id", array("controller"=>"blog", "action"=>"show"));
+      WaxUrl::perform_mappings();
+      $this->assertEqual(WaxUrl::get("controller"), "blog");
+      $this->assertEqual(WaxUrl::get("category"), "tech");
+      $this->assertEqual(WaxUrl::get("id"), "5");
+    }
     
     public function test_wildcard_map() {
       $_GET["route"]="article/tech/humour/pics";
@@ -38,6 +39,11 @@ class TestWaxUrl extends WXTestCase {
       WaxUrl::perform_mappings();
       $this->assertTrue(is_array(WaxUrl::get("tags")) );
       $this->assertEqual(count(WaxUrl::get("tags")), 3);
+    }
+    
+    public function test_formats() {
+      $_GET["route"]="sitemap.xml";
+      WaxUrl::perform_mappings();
     }
     
    
