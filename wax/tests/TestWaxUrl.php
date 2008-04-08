c911acdfe43daff9cec94b2af030d276f2a82153
Ross Riley <rossriley@Macintosh.local>
2008-04-08 16:46:07 +0100

    Tests on WaxModelField

--- wax/tests/TestWaxUrl.php
+++ wax/tests/TestWaxUrl.php
@@ -6,7 +6,9 @@ class TestWaxUrl extends WXTestCase {
     }
     
     public function tearDown() {
-     
+      error_log("-------------")
+      print_r($_GET);
+      error_log("-------------")
     }
     
     public function test_basic_map() {
