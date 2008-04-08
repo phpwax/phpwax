0f36d8495a9b6003a818b83bb3cfb75cbb6f0364
Ross Riley <rossriley@Macintosh.local>
2008-04-08 13:51:28 +0100

    Tests on WaxModelField

--- wax/db/WaxModelAssociation.php
+++ wax/db/WaxModelAssociation.php
@@ -23,6 +23,8 @@ class WaxModelAssociation extends WaxRecordset {
   
   
   public function __call($method, $args) {
+    error_log($method);
+    error_log($args);
     return $this->model->get_col($this->owner_field)->$method($args);
   }
 
