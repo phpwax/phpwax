dd7c85c4b8b8947e3f8a90505d5eea230c69fdfc
Ross Riley <rossriley@Macintosh.local>
2008-04-08 13:17:32 +0100

    Tests on WaxModelField

--- wax/db/fields/HasManyField.php
+++ wax/db/fields/HasManyField.php
@@ -26,7 +26,7 @@ class HasManyField extends WaxModelField {
   public function get() {
     $model = new $this->model_name();
     //return $model->filter(array($this->join_field=>$this->model->primval))->all();
-    return new WaxModelAssociation($model->filter(array($this->join_field=>$this->model->primval) ) ), $this->model, $this->field);
+    return new WaxModelAssociation($model->filter(array($this->join_field=>$this->model->primval) ) , $this->model, $this->field);
   }
   
   public function set($value) {
