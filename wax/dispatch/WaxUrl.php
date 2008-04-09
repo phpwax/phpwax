00b4a59130c31c27054f1bec1082d2292d6270e0
Ross Riley <rossriley@Macintosh.local>
2008-04-09 18:21:35 +0100

    Work on 404 redirects Work on differential environments Fix to WaxUrl bug

--- wax/dispatch/WaxUrl.php
+++ wax/dispatch/WaxUrl.php
@@ -128,6 +128,7 @@ class WaxUrl {
 	  }
 	  if($controller) {
 	    $_GET["controller"]=$controller;
+	    $_GET["route"]=str_replace($controller, "", $_GET["route"]);
 	    $_GET["route"]=ltrim($_GET["route"], "/");
 	  }
 	}
