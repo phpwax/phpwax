6892f2361fbf1f7acfb045cfbfd23ddad8ef568c
Ross Riley <rossriley@Macintosh.local>
2008-04-10 13:08:35 +0100

    Cleaned up default exception handling

--- wax/exceptions/WaxDbException.php
+++ wax/exceptions/WaxDbException.php
@@ -9,7 +9,7 @@ class WaxDbException extends WXException {
   public $help = "The application couldn't initialise a database connection using the following settings:";
   
 	function __construct( $message, $code, $db_settings = array() ) {
-	  $this->help .= "<br /><pre>".syck_dump($db_settings)."</pre>";
+	  $this->help .= "<br /><pre>".print_r($db_settings, 1)."</pre>";
   	parent::__construct( $message, $code);
   }
 }
