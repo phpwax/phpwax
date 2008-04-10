29312eea821a0f05d2b3e03c19a54927ae3274d4
Ross Riley <rossriley@Macintosh.local>
2008-04-10 14:19:14 +0100

    Wrapper request object

---
+++ wax/tests/TestRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+class TestRequest extends WXTestCase {
+    public function setUp() {
+      $_GET = array();
+    }
+    
+    public function tearDown() {}
+    
+    public function test_basic_get() {
+      $_GET["test"]="<SCRIPT SRC=http://ha.ckers.org/xss.js></SCRIPT>";
+      echo Request::$test;
+    }
+   
+}
+
+
+
+
+
+
+
