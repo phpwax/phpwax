59fe57223fdce3d5c77bc711e146f02320ada6f9
Ross Riley <rossriley@Macintosh.local>
2008-04-10 12:32:08 +0100

    Cleaned up default exception handling

--- wax/exceptions/WXException.php
+++ wax/exceptions/WXException.php
@@ -22,8 +22,8 @@ class WXException extends Exception
 	public function __construct($message, $heading, $code = "500") {
     parent::__construct($message, $code);
     $this->error_heading = $heading;
-    $this->error_message = $this->format_trace($this);
 		$this->cli_error_message = $this->cli_format_trace($this);
+    $this->error_message = $this->format_trace($this);
 		if(defined('CLI_ENV')) $this->cli_giveup();
 		else $this->handle_error();
   }
