642f97fa87241623cc49403267c2f88f9d5c94b9
Ross Riley <rossriley@Macintosh.local>
2008-04-10 14:36:45 +0100

    Wrapper request object

--- wax/tests/TestRequest.php
+++ wax/tests/TestRequest.php
@@ -8,8 +8,8 @@ class TestRequest extends WXTestCase {
     public function tearDown() {}
     
     public function test_basic_get() {
-      $_GET["test"]="<SCRIPT SRC=http://ha.ckers.org/xss.js></SCRIPT>";
-      $this->assertNotEqual(Request::get("test"), Request::raw("test"));
+      $_GET["test"]="hello";
+      $this->assertEqual(Request::get("test"), "hello");
     }
    
 }
