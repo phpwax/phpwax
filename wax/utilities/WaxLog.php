a0bd480cfa9841ccb32b5005784ada6495943eb3
Ross Riley <rossriley@Macintosh.home>
2008-04-11 02:56:56 +0100

    Work on tests for paginated recordset

--- wax/utilities/WaxLog.php
+++ wax/utilities/WaxLog.php
@@ -10,40 +10,19 @@
 class WaxLog {
 
   
-  static public $log_file=false;
-
-  static public $logs = array();
-  static public $logs_enabled = array();
-  static public $log_handler = array("WaxLog", "write");
-  static public $auto_flush = true;
+  static $log_file=false;
+  static $log_handler = array("WaxLog", "log");
   
   static public function add($type, $message) {
     if(!self::$log_file) self::$log_file = LOG_DIR.ENV.".log";
     ini_set("error_log",self::$log_file);
-    if(in_array( $type, self::$logs_enabled)) self::$logs[]=array($type, $message);
-    if(self::$auto_flush) call_user_func(self::$log_handler, self::output());
+    call_user_func(self::$log_handler, self::output());
   }
   
-  static public function log($type) {
-    self::$logs_enabled[$type]=true;
-  }
-  
-  public function output() {
-    $output = array();
-    foreach(self::$logs as $log) {
-      $output[]= "[".$log[0]."] ". $log[1];
+  static public function log($type, $output) {
+    if(defined("DEBUG_".strtoupper($type))) {
+      error_log("[$type] $output");
     }
-    self::flush();
-    return $output;
-  }
-  
-  public function flush() {
-    self::$logs=array();
-  }
- 
-  
-  public function write($output) {
-    foreach($output as $log) error_log($log);
   }
   
 
