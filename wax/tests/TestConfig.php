0cdc606e3631e68c747a608215121d6ae4427622
Ross Riley <rossriley@Macintosh.home>
2008-04-12 11:27:36 +0100

    Clean up of Config class and rewrite of tests

---
+++ wax/tests/TestConfig.php
@@ -0,0 +1,33 @@
+<?php
+
+class TestConfig extends WXTestCase 
+{
+    public function setUp() {
+      Config::set_environment('test');
+    }
+    
+    public function tearDown() {}
+
+  	public function test_set_array() {
+  	  Config::set(array("newtest"=>"5"));
+  	  $this->assertEqual(Config::get('newtest'), 5);
+  	}
+  	
+  	public function test_set() {
+  	  Config::set("myconf", "myval");
+  	  $this->assertEqual(Config::get('myconf'), "myval");
+  	}
+
+    public function test_get($value) { 
+  	  $config = Config::get('all');
+  	  $this->assertTrue(is_array($config));
+  	}
+
+  	public function test_set_environment($env) {
+  	  Config::set_environment('test');
+  	  $this->assertEqual(Config::get('test/db'), Config::get('db'));
+  	}
+
+    
+}
+?>
