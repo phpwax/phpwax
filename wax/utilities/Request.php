842eab9aa48358c7e22e262dc4cdd5cfc2e8f4f3
Ross Riley <rossriley@Macintosh.home>
2008-04-13 12:28:33 +0100

    Clean up of Config class and rewrite of tests

--- wax/utilities/Request.php
+++ wax/utilities/Request.php
@@ -12,20 +12,35 @@ class Request {
 	static $params = false;
 	static $get = false;
 	static $post = false;
+	static $init = false;
 	
 	public function filter($val) {
 	  return filter_var($val, FILTER_FLAG_ENCODE_HIGH);
 	}
 	
+	public function init() {
+	  if(!self::$init) {
+	    self::$get = WaxUrl::get_params();
+	    self::$post = $_POST;
+	    self::$params = array_merge(self::$get,self::$post);
+	    self::$init = true;
+	  }
+	}
+	
 	
 	public function get($name) {
-	  if(!self::$get) self::$get = WaxUrl::get_params();
+	  self::init();
 	  return self::$get[$name];
 	}
 	
 	public function post($name) {
-	  if(!self::$post) self::$post = $_POST;
+	  self::init();
 	  return self::$post[$name];
+	}
+	
+	public function param($name) {
+	  self::init();
+	  return self::$params[$name];
 	}
 
 
