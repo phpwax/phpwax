d8009801df07ef6deb4caabeee7e60f3ccb44898
Ross Riley <ross@oneblackbear.com>
2008-04-15 17:53:45 +0100

    Fix to catch all url parameters

--- wax/db/WaxModel.php
+++ wax/db/WaxModel.php
@@ -42,6 +42,7 @@ class WaxModel {
  		}
  		if($params) {
  		  $res = $this->filter(array($this->primary_key => $params))->first();
+ 		  $this->row=$res->row;
  		}
  		$this->define($this->primary_key, $this->primary_type, $this->primary_options);
  		$this->setup();
