adf8a2e1dcdc935ae9b5c79334fa6b9bf8a82e83
Ross Riley <ross@oneblackbear.com>
2008-04-16 09:13:41 +0100

    Work on widget output

--- wax/forms/widgets/SelectInput.php
+++ wax/forms/widgets/SelectInput.php
@@ -23,7 +23,7 @@ class SelectInput extends WaxWidget {
   public function render() {
     $out ="";
     if($this->show_label) $out .= sprintf($this->label_template, $this->attributes["id"], $this->label); 
-    $out .= sprintf($this->template, $this->make_attributes());
+    $out .= sprintf($this->template, $this->make_attributes(), $this->make_choices());
     return $out;
   }
   
@@ -50,6 +50,7 @@ class SelectInput extends WaxWidget {
       }
       
     }
+    return $output;
   }
   
 
