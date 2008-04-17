5ee882c01ab22a35c0a1da115b9102ee4ca3f7a7
Ross Riley <ross@oneblackbear.com>
2008-04-17 12:32:08 +0100

    Added error sniffing to widgets

--- wax/forms/WaxWidget.php
+++ wax/forms/WaxWidget.php
@@ -17,7 +17,7 @@ class WaxWidget {
   public $help_text = false;
   public $label_template = '<label for="%s>%s</label>';
   public $template = '<input %s />';
-  public $error_message = false;
+  public $error_messages = false;
   public $error_template = '<span class="error_message">%s</span>';
   
   public function __construct($name, WaxModel $model=null) {
@@ -34,17 +34,19 @@ class WaxWidget {
       $this->label = $model_field->label;
       $this->help_text = $model_field->help_text;
       if($this->label===true) $this->label = Inflections::humanize($name);
-      if($er = $model->errors[$name]) $this->error_message = $er;
+      if($er = $model->errors[$name]) $this->error_messages = (array)$er;
     }
   }
   
   
   public function render() {
     $out ="";
-    if($this->error_message) $this->attributes["class"].=" error_field";
+    if($this->error_messages) $this->attributes["class"].=" error_field";
     if($this->label) $out .= sprintf($this->label_template, $this->attributes["id"], $this->label); 
     $out .= sprintf($this->template, $this->make_attributes());
-    if($this->error_message) $out .= sprintf($this->error_template, $this->error_message);
+    if($this->error_messages) {
+      foreach($this->error_messages as $error) $out .= sprintf($this->error_template, $this->error_message);
+    }
     return $out;
   }
   
