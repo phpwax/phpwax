4a15eea12d0aac095fbcffc53ae77fccd37fb338
Ross Riley <ross@oneblackbear.com>
2008-04-17 12:34:45 +0100

    Added error sniffing to widgets

--- wax/forms/widgets/TextInput.php
+++ wax/forms/widgets/TextInput.php
@@ -20,30 +20,6 @@ class TextInput extends WaxWidget {
   public $label_template = '<label for="%s">%s</label>';
   public $template = '<input %s />';
   
-  
-  
-  public function render() {
-    $out ="";
-    if($this->error_message) $this->attributes["class"].=" error_field";
-    if($this->label) $out .= sprintf($this->label_template, $this->attributes["id"], $this->label); 
-    $out .= sprintf($this->template, $this->make_attributes());
-    if($this->error_message) $out .= sprintf($this->error_template, $this->error_message);
-    return $out;
-  }
-  
-  public function attribute($name, $value) {
-    $this->attributes[$name]=$value;
-  }
-  
-  public function make_attributes() {
-    $res = "";
-    foreach($this->attributes as $name=>$value) {
-      $res.=sprintf('%s="%s" ', $name, $value);
-    }
-    return $res;
-  }
-  
-
 
 
 } // END class
