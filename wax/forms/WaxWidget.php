d2a6c0b88d5a815bfe7c1f89f4b43015bcc44212
Ross Riley <ross@oneblackbear.com>
2008-04-19 15:59:10 +0100

    Fix to bad class name call

--- wax/forms/WaxWidget.php
+++ wax/forms/WaxWidget.php
@@ -71,7 +71,7 @@ class WaxWidget {
   public function make_attributes() {
     $res = "";
     foreach($this->allowable_attributes as $name=>$value) {
-      if($this->$name) $res.=sprintf('%s="%s" ', $name, $value);
+      if(isset($this->$name)) $res.=sprintf('%s="%s" ', $name, $value);
     }
     return $res;
   }
