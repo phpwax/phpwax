daa9b6887012a93b926fd075f0c29cd109f8c46d
Ross Riley <ross@oneblackbear.com>
2008-04-20 14:07:45 +0100

    Work on linking widget directly to model

--- wax/forms/widgets/SelectInput.php
+++ wax/forms/widgets/SelectInput.php
@@ -16,13 +16,9 @@ class SelectInput extends WaxWidget {
   public $class = "input_field select_field";
   public $label_template = '<label for="%s">%s</label>';
   public $template = '<select %s>%s</select>';
-  
+
   
   public function tag_content() {
-    return $this->make_choices();
-  }
-  
-  public function make_choices() {
     $output = "";
     $choice = '<option value="%s"%s>%s</option>';
     foreach($this->choices as $value=>$option) {
