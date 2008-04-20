b6bf51cd4619cf85bf715bcc3e1e59d787d9b533
Ross Riley <ross@oneblackbear.com>
2008-04-20 13:34:06 +0100

    Work on linking widget directly to model

--- wax/forms/WaxWidget.php
+++ wax/forms/WaxWidget.php
@@ -42,7 +42,7 @@ class WaxWidget {
   public function make_attributes() {
     $res = "";
     foreach($this->allowable_attributes as $name) {
-      $res.=sprintf('%s="%s" ', $name, $this->{$name});
+      if($this->{$name}) $res.=sprintf('%s="%s" ', $name, $this->{$name});
     }
     return $res;
   }
