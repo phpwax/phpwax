471d831400c7f66493bd1da7078b03168b62c55d
Sheldon Els <sheldon@oneblackbear.com>
2008-05-27 17:31:28 +0100

    dbadapter insert temporary revert to fix ManyToMany Field tests

--- wax/db/WaxDbAdapter.php
+++ wax/db/WaxDbAdapter.php
@@ -56,9 +56,13 @@ abstract class WaxDbAdapter {
     $stmt = $this->db->prepare("INSERT into `{$model->table}` (".join(",", array_keys($model->row)).") 
       VALUES (".join(",", array_keys($this->bindings($model->row))).")");
     $stmt = $this->exec($stmt, $model->row);
+    
+    //changed back to an old method temporarily
     $new = array($model->primary_key => $this->db->lastInsertId());
-    $class_name =  get_class($model) ;
-    return new $class_name($this->db->lastInsertId());
+    return $model->clear()->filter($new)->first();
+    
+    //$class_name = get_class($model);
+    //return new $class_name($this->db->lastInsertId());
 	}
   
   public function update(WaxModel $model) {
