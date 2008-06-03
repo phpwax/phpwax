e8ba9d76eaaf8132075bc816f784f3a149201144
charles <charles@oneblackbear.com>
2008-06-03 14:28:07 +0100

    tentative routing fix

--- wax/exceptions/WXRoutingException.php
+++ wax/exceptions/WXRoutingException.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package PHP-Wax
- * @author Ross Riley
+ * @author Ross Riley & charles marshall
  **/
 class WXRoutingException extends WXException
 {
@@ -10,12 +10,15 @@ class WXRoutingException extends WXException
   static $double_redirect = false;
   
 	function __construct( $message, $code="Page cannot be found", $status = "404" ) {  	
+		
   	if($location = self::$redirect_on_error) {
   	  $this->simple_routing_error_log();
   	  if(!self::$double_redirect) {
   	    self::$double_redirect = true;
         header("HTTP/1.1 404 Not Found",1, 404);  
         $_GET["route"]=$location;
+				WaxUrl::$params = false;
+				WaxUrl::perform_mappings();
         $delegate = Inflections::slashcamelize(WaxUrl::get("controller"), true)."Controller";
   		  $delegate_controller = new $delegate;
   		  $delegate_controller->execute_request();
