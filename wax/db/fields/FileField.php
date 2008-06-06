9313156d2980d7830474b918f7ab8d43c6b3f936
charles <charles@oneblackbear.com>
2008-06-06 16:39:35 +0100

    update

--- wax/db/fields/FileField.php
+++ wax/db/fields/FileField.php
@@ -41,9 +41,10 @@ class FileField extends WaxModelField {
 	public function valid_extension(){
 		$file = $_FILES[$this->model->table];
 		$name= $file['name'][$this->col_name];
-		$ext = strtolower(substr($name, strrpos($name, ".") ));
-		if($this->allowed_extensions && !in_array($ext, $this->allowed_extensions)  ) $this->add_error($this->field, sprintf($this->messages["format"], $ext));
-
+		if(strlen($name)){
+			$ext = strtolower(substr($name, strrpos($name, ".") ));
+			if($this->allowed_extensions && !in_array($ext, $this->allowed_extensions)  ) $this->add_error($this->field, sprintf($this->messages["format"], $ext));
+		}
 	}
 	/**** overides *****/
 
