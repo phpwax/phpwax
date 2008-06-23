2e67d0dc2eaca054fde33d81a1bda8bcd954eb0a
Ross Riley <ross@oneblackbear.com>
2008-06-23 10:52:25 +0100

    Made manytomany field not editable by default

--- wax/db/fields/ManyToManyField.php
+++ wax/db/fields/ManyToManyField.php
@@ -8,6 +8,7 @@
 class ManyToManyField extends WaxModelField {
   
   public $maxlength = "11";
+  public $editable = false;
   public $target_model = false; //model on the other side of the many to many
   public $join_model = false; //instance of WaxModelJoin filtered with this instance's primary key
   
