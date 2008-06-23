960bb794379c316fa04d95363da673cdbc2348bd
Ross Riley <ross@oneblackbear.com>
2008-06-23 11:36:14 +0100

    Added form support for ManyToMany Fields

--- wax/forms/WaxForm.php
+++ wax/forms/WaxForm.php
@@ -97,6 +97,7 @@ class WaxForm implements Iterator {
 
    public function valid() {
      $cur = $this->current();
+     print_r($cur->bound_data); exit;
      if($cur && $cur->bound_data->editable) return true;
      return false;
    }
