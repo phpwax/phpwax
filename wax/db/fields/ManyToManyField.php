2ec56aad68ce794a93bbd1a3f23ac4cd2298faad
charles <charles@oneblackbear.com>
2008-06-26 13:59:11 +0100

    added left join functionality on many to manys

--- wax/db/fields/ManyToManyField.php
+++ wax/db/fields/ManyToManyField.php
@@ -11,7 +11,8 @@ class ManyToManyField extends WaxModelField {
   public $target_model = false; //model on the other side of the many to many
   public $join_model = false; //instance of WaxModelJoin filtered with this instance's primary key
   public $widget = "MultipleSelectInput";
-  
+  public $use_join_select = true;
+
   public function setup() {
     $this->col_name = false;
     if(!$this->target_model) $this->target_model = Inflections::camelize($this->field, true);
@@ -32,17 +33,40 @@ class ManyToManyField extends WaxModelField {
   public function validate() {
     return true;
   }
-  
+  /**
+   * right, this is now has 2 behaves, by default 'use_join_select' is on so the following happens:
+   *  - uses the new join functionality to link the join table to the target table
+   *  - creates a join condition based on the keys on both sides
+	 *  - restricts the selected data to the one side of the join
+	 *  - returns a WaxModelAssociation of the results
+	 * However, this can be turned off and used in the old manner (more suited if lots of data; ie moves load from db to application)
+	 *  - finds all records in join table
+	 *  - creates a set of filters based on these results
+	 *  - return the WaxModelAssociation of matching values on the target table
+	 *
+	 * The reason for having two methods is that the join version removes an all call, saving one more db query, but the join sql
+	 * can be slow on large amounts of data, so the other method is here for that purpose.
+   */  
   public function get() {
-    $vals = $this->join_model->all();
-    $links = new $this->target_model;
-    if(!$vals->count()){
-      //filter added that never evaluates since we want none of the target model's to return
-      $links->filter("1 = 2");
-      return new WaxRecordset($links, array());
-    }
-    foreach($vals as $val) $filters[]= $links->primary_key."=".$val->{$this->join_field($links)};
-    return new WaxModelAssociation($links->filter("(".join(" OR ", $filters).")"), $this->model, $this->field);
+		$links = new $this->target_model;
+		if($this->use_join_select){
+			//link the table
+			$conditions = "(".$links->table.".".$links->primary_key."=".$this->join_model->table.".".$this->join_field($links) ." AND ";
+			$conditions.= $this->join_model->table.".".$this->join_field($this->model)."=".$this->model->primval .")";
+			$this->join_model->select_columns = array($links->table.".*");
+			$res = $this->join_model->clear()->left_join($links->table)->join_condition($conditions)->filter($links->table.".".$links->primary_key . " > 0");
+			return new WaxModelAssociation($res, $this->model, $this->field);
+		}else{
+			$vals = $this->join_model->all();
+    	$links = new $this->target_model;
+    	if(!$vals->count()){
+      	//filter added that never evaluates since we want none of the target model's to return
+      	$links->filter("1 = 2");
+      	return new WaxRecordset($links, array());
+    	}	
+    	foreach($vals as $val) $filters[]= $links->primary_key."=".$val->{$this->join_field($links)};
+    	return new WaxModelAssociation($links->filter("(".join(" OR ", $filters).")"), $this->model, $this->field);
+		}
   }
   
   public function set($value) {
