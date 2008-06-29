f77a0ad4e41287bbaf3047c23a1ef7fe76e24fa4
Ross Riley <ross@oneblackbear.com>
2008-06-29 19:02:49 +0100

    attempt to debug ManyToMany get recursion

--- wax/db/WaxModelAssociation.php
+++ wax/db/WaxModelAssociation.php
@@ -25,6 +25,7 @@ class WaxModelAssociation extends WaxRecordset {
   
   public function offsetGet($offset) {
     $model = get_class($this->target_model);
+    die($model);
     if(is_numeric($this->rowset[$offset])) return new $model($this->rowset[$offset]);
     $obj = clone $this->model;
     $obj->set_attributes($this->rowset[$offset]);
