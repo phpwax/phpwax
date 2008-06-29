ceca7b71b46a54e3f245987ebf6075a10df1ac4a
Ross Riley <ross@oneblackbear.com>
2008-06-29 18:28:56 +0100

    attempt to debug ManyToMany get recursion

--- wax/db/fields/ManyToManyField.php
+++ wax/db/fields/ManyToManyField.php
@@ -184,6 +184,7 @@ class ManyToManyField extends WaxModelField {
     foreach($this->join_model->rows() as $row) {
       $ids[]=$row[$this->join_model->right_field];
     }
+    print_r($ids); exit;
     return new WaxModelAssociation($target_model, $this->model, $ids);
   }
   
