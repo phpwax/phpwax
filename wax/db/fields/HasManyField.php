512d1f53536208f2c7eced76b08da39c27863b99
Ross Riley <ross@oneblackbear.com>
2008-06-29 16:32:45 +0100

    Fix to excessive querying in HasManyField get

--- wax/db/fields/HasManyField.php
+++ wax/db/fields/HasManyField.php
@@ -26,6 +26,7 @@ class HasManyField extends WaxModelField {
   
   public function get() {
     $model = new $this->target_model();
+    return $model->filter(array($this->join_field=>$this->model->primval));
     return new WaxModelAssociation($model, $this->model, $model->filter(array($this->join_field=>$this->model->primval))->all()->rowset, $this->field);
   }
   
