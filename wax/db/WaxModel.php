0bc92b0132da578bdd2f239771067a700cfe602d
Ross Riley <ross@oneblackbear.com>
2008-06-29 21:30:49 +0100

    attempt to debug TreeModel errors

--- wax/db/WaxModel.php
+++ wax/db/WaxModel.php
@@ -154,7 +154,7 @@ class WaxModel {
  	}
 
   public function get_col($name) {
-    error_log("GETTING COLUMN $name which is type of ".get_class($name) );
+    error_log("GETTING COLUMN $name which is type of ".$this->columns[$name][0] );
     return new $this->columns[$name][0]($name, $this, $this->columns[$name][1]);
   }
   
