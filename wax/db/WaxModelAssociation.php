20cb3746ecb9ba32c7d766fb45356e95060c73dc
Ross Riley <ross@oneblackbear.com>
2008-06-30 11:59:51 +0100

    attempt to debug ManyToMany get recursion

--- wax/db/WaxModelAssociation.php
+++ wax/db/WaxModelAssociation.php
@@ -9,24 +9,24 @@
 
 class WaxModelAssociation extends WaxRecordset {
   
-  public $join_model;
+  public $target_model;
   public $owner_field;
 
-  public function __construct(WaxModel $model, WaxModel $join_model, $rowset, $owner_field=false) {
+  public function __construct(WaxModel $model, WaxModel $target_model, $rowset, $owner_field=false) {
     $this->rowset = $rowset;
     $this->model = $model;
-    $this->join_model = $join_model;
+    $this->target_model = $target_model;
     $this->owner_field = $owner_field;
   }
   
   public function offsetGet($offset) {
-    $model = get_class($this->join_model);
+    $model = get_class($this->target_model);
     if(is_numeric($this->rowset[$offset])) return new $model($this->rowset[$offset]);
   }
 
   public function __call($method, $args) {
-    error_log("Looking for ".$this->owner_field." on ".get_class($this->join_model));
-    return call_user_func_array(array($this->join_model->get_col($this->owner_field), $method), $args);
+    error_log("Looking for ".$this->owner_field." on ".get_class($this->model));
+    return call_user_func_array(array($this->model->get_col($this->owner_field), $method), $args);
   }
   
 }
