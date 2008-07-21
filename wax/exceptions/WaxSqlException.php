a4231eccf65d2de79a46e41cb803ab28270f1ab5
Ross Riley <ross@oneblackbear.com>
2008-07-21 19:54:49 +0100

    Reorganisation of framework files

--- wax/exceptions/WaxSqlException.php
+++ wax/exceptions/WaxSqlException.php
@@ -9,7 +9,7 @@ class WaxSqlException extends WXException {
   public $help = "<p>There was a database query that could not execute:</p>";
   
 	function __construct( $message, $code, $query_error = false ) {
-	  if($query_error) $this->help .= "<pre>$query_error</pre>";
+	  if($query_error) $this->help .= " <pre>$query_error</pre>";
   	parent::__construct( $message, $code);
   }
 }
