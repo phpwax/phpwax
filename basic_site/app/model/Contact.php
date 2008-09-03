369cea9ce59f9dfe97c73dfa0d8596d490b8c7a1
charles <charles@oneblackbear.com>
2008-09-03 12:44:33 +0100

    contact model comments

--- basic_site/app/model/Contact.php
+++ basic_site/app/model/Contact.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * This class extends as built in phpwax model called WXEmail (pre release naming convention, will be updated to match new style soon)
+ * The WXEmail class handles sending of emails in a nice fashion and extending it allows you to do some nice things.
+ * 
+ * Firstly, look at app/view/contact to see the actual body of the emails being sent (the file names match the method names just like 
+ * controllers and actions). 
+ *
+ * You will have noticed that to actually send the email you can call send_<name of method> and that will sort it for you. This send prefix
+ * just simplifies the controller code.
+ *
+ * The values you assign to $this can be accessed directly in the respective view (take a look at app/view/contact/contact.txt) 
+ */
+
 class Contact extends WXEmail {
   
   public $from = "test@example.com";
