5a6e2fdc9479b8174b8d0fb0e5b561f772b88939
Ross Riley <ross@oneblackbear.com>
2008-09-09 17:51:10 +0100

    upload script

--- wax/AutoLoader.php
+++ wax/AutoLoader.php
@@ -174,7 +174,6 @@ class AutoLoader
 	  self::recursive_register(CONTROLLER_DIR, "application");
 		self::recursive_register(FRAMEWORK_DIR, "framework");
 		self::autoregister_plugins();
-		self::detect_environments();
 		self::include_from_registry('WXInflections');  // Bit of a hack -- forces the inflector functions to load
 		self::include_from_registry('WXHelpers');  // Bit of a hack -- forces the helper functions to load
 		self::register_helpers();
