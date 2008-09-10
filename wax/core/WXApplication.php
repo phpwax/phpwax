f3c8e69a1fdc2c53272d13816637c7a1cf9ea636
Ross Riley <ross@oneblackbear.com>
2008-09-10 12:59:20 +0100

    Framework bug in environment detection

--- wax/core/WXApplication.php
+++ wax/core/WXApplication.php
@@ -40,7 +40,7 @@ class WXApplication {
    */
 	private function setup_environment() {
 	  $addr = gethostbyname($_SERVER["HOSTNAME"]);
-	  if(!$addr) $addr = gethostbyname($_SERVER["HTTP_HOST"]);
+	  if(!$addr || $addr==$_SERVER["HOSTNAME"]) $addr = gethostbyname($_SERVER["HTTP_HOST"]);
 	  $regexp = '/^((1?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(1?\d{1,2}|2[0-4]\d|25[0-5])$/'; 
 	  if(!preg_match($regexp, $addr)) $addr = false;
 		if(defined('ENV')) {
