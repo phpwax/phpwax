4b8245a0ca8d01ebb8c7551c766c348dc19c1d8e
charles <charles@oneblackbear.com>
2009-03-19 14:19:51 +0000

    Michals date input - uses google load and jquery ui to render a date picker

--- forms/widgets/DateInput.php
+++ forms/widgets/DateInput.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * Date Input Widget class
  *
@@ -9,8 +8,19 @@
 class DateInput extends TextInput {
 
   public $type="text";
-  public $class = "input_field text_field date_field";
+  public $class = "input_field text_field date_field form_datepicker";
   
+  public $label_template = "<label for='%s'>%s</label>
+                    <script src='http://www.google.com/jsapi'  type='text/javascript'></script>
+                    <script type='text/javascript'>                      
+                      google.load('jquery', '1.3.2');
+                      google.load('jqueryui', '1.7.0');
+                      $(document).ready(function(){
+                        console.log('loading...');
+                      $('input.form_datepicker').datepicker({ dateFormat: 'yy-mm-dd' });
+                      });
+                    </script>";
 
 
 } // END class
+?>
