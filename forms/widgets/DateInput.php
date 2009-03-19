aefe49142c6a08c425adb9c7191fd21226d9dce8
charles <charles@oneblackbear.com>
2009-03-19 16:15:26 +0000

    just use 1.3, less exact versions so it will update

--- forms/widgets/DateInput.php
+++ forms/widgets/DateInput.php
@@ -13,8 +13,8 @@ class DateInput extends TextInput {
   public $label_template = "<label for='%s'>%s</label>
                     <script src='http://www.google.com/jsapi'  type='text/javascript'></script>
                     <script type='text/javascript'>                      
-                      google.load('jquery', '1.3.2');
-                      google.load('jqueryui', '1.7.0');
+                      google.load('jquery', '1.3');
+                      google.load('jqueryui', '1.7');
                       $(document).ready(function(){
                       $('input.form_datepicker').datepicker({ dateFormat: 'yy-mm-dd' });
                       });
