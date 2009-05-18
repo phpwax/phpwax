25cd49979228e619ddaa7d6d66575432191ca19c
charles <charles@oneblackbear.com>
2009-05-18 12:09:19 +0100

    bug fix - is writeable check on directory

--- cache/engines/WaxCacheFile.php
+++ cache/engines/WaxCacheFile.php
@@ -29,7 +29,7 @@ class WaxCacheFile {
 	
 	public function set($value) {
 	  WaxLog::log("info", "[CACHE] Writing cache file for ".$this->label);
-	  if(is_writable($this->file())) return file_put_contents($this->file(), $value);
+	  if(is_writable($this->cache_dir)) return file_put_contents($this->file(), $value);
 	  else {
 	    WaxLog::log("error", "[CACHE] Cache files could not be written. Check permissions on '".$this->cache_dir."'");
 	    return false;
