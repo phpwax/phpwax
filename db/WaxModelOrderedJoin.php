9394a6ea4671108dda88fd5ebefc86ca0d465500
Sheldon Els <sheldon.els@gmail.com>
2009-06-10 16:54:02 +0100

    an ordered join class

---
+++ db/WaxModelOrderedJoin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Same as a Join, but with order
+*/
+class WaxModelOrderedJoin extends WaxModelJoin {
+  public function setup() {
+    parent::setup();
+    $this->define("join_order", "IntegerField");
+  }
+}
+
+?>
