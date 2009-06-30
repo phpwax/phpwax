e151be8c86f7cfb6bdee364f889fdbef475bdf9f
charlesmarshall <charles@oneblackbear.com>
2009-06-30 11:11:09 +0100

    env update

--- examples/skel/app/config/environment.php
+++ examples/skel/app/config/environment.php
@@ -6,9 +6,19 @@
  */
 
 define('WAX_ROOT', dirname(dirname(dirname(__FILE__)))."/" );
+/***
+ * uncomment this line if you want an absolute path and replace the constant with the path to your
+ * you wax folder
+ */ 
+//define('WAX_ROOT', "/path/to/wax/" );
+/**
+ * to change the relative path of wax folder then set this constant to the location you want
+ * remember this has to be relative to the wax_root set above..
+ */ 
 define('WAX_DIR', 'wax');
 if(is_dir(WAX_ROOT.WAX_DIR)) define('FRAMEWORK_DIR', WAX_ROOT.WAX_DIR);
 elseif(is_dir(WAX_ROOT."phpwax")) define('FRAMEWORK_DIR', WAX_ROOT."phpwax");
+else throw new Exception('PHP WAX folder does not exist!');
 
 ini_set('include_path', ini_get("include_path").":".WAX_ROOT);
 
