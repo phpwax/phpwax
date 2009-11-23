84307bdadec3e5b8514fde874d0000b4f714d729
charlesmarshall <cm56marshall@googlemail.com>
2009-11-23 12:59:50 +0000

    removed the validate unique from here as it didnt work & meant that only fields that extended char fields could be unique.. so no unique numberic fields.. I've replace it with a function in the wax validations class

--- db/fields/CharField.php
+++ db/fields/CharField.php
@@ -13,25 +13,7 @@ class CharField extends WaxModelField {
     "unique"=>      "%s has already been taken"
   );
   
-  public function setup() {
-    
-  }
+  public function setup() {}
 
-  public function validate() {    
- 	  $this->valid_unique();
-  }
-
-	protected function valid_unique() {
-		if($this->unique){
-			$current_value = $this->model->{$this->col_name};
-			$primary = $this->model->primary_key;
-			//clone model to not mess with filters
-			$model_name = get_class($this->model);
-			$model = new $model_name;
-			//find anything that matches this column value, make sure primay key is not this one
-			$present = $model->filter("`".$this->col_name."`='".$current_value."'")->filter("`".$primary."` <> '".$this->model->$primary."'")->all();
-			if($present->count() > 0) $this->add_error($this->field, sprintf($this->messages["unique"], $this->label));
-		}
-  }
 
 }
