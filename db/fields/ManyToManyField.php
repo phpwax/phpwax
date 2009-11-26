5aafcec3580158839fb6b97cb22194c90c1e11d3
Ross Riley <ross@oneblackbear.com>
2009-11-26 12:28:59 +0000

    Bug fix

--- db/fields/ManyToManyField.php
+++ db/fields/ManyToManyField.php
@@ -79,10 +79,11 @@ class ManyToManyField extends WaxModelField {
 		  $this->join_model->select_columns[] = "{$this->join_model->table}.$col";
 		  
 		$cache = WaxModel::get_cache(get_class($this->model), $this->field, $this->model->primval.":".md5(serialize($target->filters)),$vals->rowset, false);
-		if($cache) return new WaxModelAssociation($this->model, $target, $cache, $this->field);
-		
+		if($cache) return new WaxModelAssociation($this->model, $target, $cache, $this->field);		
 		$vals = $this->join_model->all();
+		
 		WaxModel::set_cache(get_class($this->model), $this->field, $this->model->primval.":".md5(serialize($target->filters)), $vals->rowset);
+		
 		return new WaxModelAssociation($this->model, $target, $vals->rowset, $this->field);
   }
   
