7f5312eb977c6ed4057c63af918bd4eeba6c1696
Sheldon <sheldon.els@gmail.com>
2009-11-26 08:54:40 +0000

    big commit: updated closure tree, pretty much all the needed stuff working except preorder traversal.

--- db/WaxClosureTable.php
+++ db/WaxClosureTable.php
@@ -6,10 +6,28 @@
  *
  **/
 class WaxClosureTable extends WaxModel {
+  public $closure_tree_class = false;
+  
+  /**
+   * allow class name of closure to be passed in
+   */
+ 	function __construct($params=null,$closure_tree_class=null) {
+    $this->closure_tree_class = $closure_tree_class;
+    parent::__construct($params);
+  }
+  
   public function setup(){
-    $this->define("ancestor","ForeignKey",array("col_name"=>"ancestor","target_model"=>"WaxClosureTree"));
-    $this->define("descendant","ForeignKey",array("col_name"=>"descendant","target_model"=>"WaxClosureTree"));
+    $this->define("ancestor","ForeignKey",array("col_name"=>"ancestor_id","target_model"=>$this->closure_tree_class));
+    $this->define("descendant","ForeignKey",array("col_name"=>"descendant_id","target_model"=>$this->closure_tree_class));
     $this->define("level","IntegerField");
+  }
+
+  /**
+   * stop sync from syncing base class
+   */
+  public function syncdb(){
+    if($this->table == "wax_closure_table") return;
+    parent::syncdb();
   }
 }
 ?>
--- db/WaxClosureTree.php
+++ db/WaxClosureTree.php
@@ -8,129 +8,175 @@
  **/
 class WaxClosureTree extends WaxModel {
   public $closure_table_class = "WaxClosureTable";
-  public $closure_table = false;
   
- 	function __construct($params=null) {
-    parent::__construct($params);
-    $this->closure_table = new $this->closure_table_class;
-    $this->closure_table->table = $this->table."_closure_table";
+  /**
+   * returns an empty closure table model
+   */
+  private function closure_table(){
+    $closure_table = new $this->closure_table_class(null,get_class($this));
+    $closure_table->table = $this->table."_closure_table";
+    return $closure_table;
   }
+  
+  /**
+   * returns a WaxRecordSet based on the path from this node back to it's root
+   * accepts a depth parameter which sets how far back in the ancestry you want to return, if left null it will return all the way back to the root
+   */
+  public function ancestors($depth = null){
+    $res = clone $this;
+		$res->clear();
+		$res->left_join($this->closure_table());
+		$res->join_condition("$this->table.$this->primary_key = ancestor_id");
+		$res->select_columns = array($this->table.".*");
+		$res->order("level");
+    $res->filter("descendant_id",$this->primval());
+    if($depth !== null) $this->limit($depth + 1);
+    return $res->all();
+  }
+  
+  /**
+   * returns the parent of the current node
+   */
+  public function parent(){
+    return $this->ancestors(1)->end();
+  }
+
+  /**
+   * returns a WaxRecordSet of all the descendants of the current node, it is effectively the entire subtree of the given node
+   * accepts a depth parameter which sets how deep in the subtree you want to return, if left null it will return all the way to bottom
+   */
+  public function descendants($depth = null){
+    $res = clone $this;
+		$res->clear();
+		$res->left_join($this->closure_table());
+		$res->join_condition("$this->table.$this->primary_key = descendant_id");
+		$res->select_columns = array($this->table.".*");
+		$res->order("level");
+		$res->filter("ancestor_id",$this->primval());
+    if($depth !== null) $this->filter("level",$depth,"<=");
+    return $res->all();
+  }
+
+  /**
+   * returns the direct children of the current node
+   */
+  public function children(){
+    $res = $this->descendants(1); //fetch 1 level of descendants
+    array_shift($res); //drop off itself
+    return $res;
+  }
+  
   /**
    * used to implement setting parent or children
    * makes sure that anything in the ancestors doesn't exist in the subtree, causing recursion
-   *
-   * @return void
    */
 	public function __set($name, $value){
-	  if($name == "parent"){
-	    //first isolate the subtree, removing all descendants of the node you're reparenting from all ancestors of it's original parent
-	    $this->closure_table->clear()->filter("ancestor",$this->parent()->ancestors())->filter("descendants",$this->descendants())->delete();
-	    //add each descendant to each ancestor
-	    //INSERT INTO CLOSURE_TABLE cross product of $value->ancestors() and $this->descendants()
-	    //this way is super slow, need a way to do the inserts as 1 query
-	    foreach($value->ancestors() as $ancestor)
-	      foreach($this->descendants() as $descendant)
-	        $this->closure_table->clear()->create(array("ancestor"=>$ancestor,"descendant"=>$descendant));
-	  }elseif($name == "children"){
-	    if($value instanceof Iterator) foreach($value as $node) $this->__set("children",$node);
-	    //get all the ancestors of the current and assign them as ancestors of the new node
-	    $child_primval = $value->primval();
-	    foreach($this->path_from_root as $node) $this->closure_table->create(array("ancestor"=>$node->primval(),"descendant"=>$child_primval));
-	  }else parent::__set($name, $value);
-  }
-  /**
-   * gets the parent node of the current node
-   *
-   * @return WaxClosureTree
-   */
-  public function parent(){
-  }
-  /**
-   * gets the direct children of the current node
-   *
-   * @return WaxRecordSet
-   */
-  public function children(){
-  }
-  /**
-   * function to get the tree structure for in-order traversal via a foreach($model->tree() as $node) type use
-   * if the current model is empty it will return the entire tree including all root nodes
-   * if the current model is a particular node (has an id) it will only return the tree underneath that node
-   * if filters are set on the model, it will return only rows which match those filters (BE WARE, THIS CAN HAVE SOME UNUSUAL RESULTS)
-   *
-   * @return
-   */
-	public function tree(){
-	}
-  /**
-   * get the root nodes
-   * now with caching! yey!
-   * @return WaxRecordSet of all the self-parented nodes or nodes with unidentifiable parents
-   */
-  public function roots() {
-  }
-
-  /**
-   * this makes a WaxRecordSet based on the path from this object back up to its root
-   * @return WaxRecordSet
-   */
-  public function path_to_root(){
-  }
-
-  /**
-   * this makes a WaxRecordSet based on the path from this object's root down to it
-   * @return WaxRecordSet
-   */
-  public function path_from_root(){
-  }
-
-  /**
-   * returns a numeric representation of this objects depth in the tree
-   * 0 is root
-   * @return integer
-   */
-  public function get_level() {
-  }
-
-  /**
-   * returns true for root nodes and false for everything else
-   * @return boolean
-   */
-  public function is_root() {
-    if($this->level === 0) return true;
-    else return false;
+	  if($name == "parent") return $this->set_parent($value);
+	  elseif($name == "children") return $this->set_children($value);
+	  else return parent::__set($name, $value);
   }
   
   /**
+   * add children to the current node
+   * returns the value it's setting (similar to how other setters in php work, setters return the value they set)
+   */
+  private function set_children($child){
+    if($child instanceof Iterator) foreach($child as $child_node) $this->set_children($child_node);
+    if($child instanceof WaxClosureTree){
+      $this->reparent($this,$child);
+      return $child;
+    }
+  }
+  
+  /**
+   * set the parent of the current node, otherwise known as reparenting or moving a sub tree
+   * returns the parent node
+   */
+  private function set_parent(WaxClosureTree $new_parent){
+    if($new_parent instanceof WaxClosureTree){
+      $this->reparent($new_parent,$this);
+      return $child;
+    }
+  }
+  
+  /**
+   * reattach a subtree to a new node
+   */
+  private function reparent(WaxClosureTree $new_parent, WaxClosureTree $subtree_root){
+    //first isolate the subtree, removing all descendants of the root node from all ancestors of it's current parent
+    //interestingly, taking out this step turns this into a graphing engine with no specific heirarchy
+    if(!$subtree_root->is_root()){
+      $ancestors = $subtree_root->ancestors();
+      array_pop($ancestors);
+      $this->closure_table()->filter("ancestor",$ancestors)->filter("descendants",$subtree_root->descendants())->delete();
+    }
+
+    //add each descendant to each ancestor
+    //INSERT INTO CLOSURE_TABLE cross product of $new_parent->ancestors() and $this->descendants()
+    //this way is super slow, need a way to do the inserts as 1 query
+    foreach($new_parent->ancestors() as $ancestor){
+      foreach($subtree_root->descendants() as $descendant){
+        $link = $this->closure_table();
+        $link->ancestor = $ancestor;
+        $link->descendant = $descendant;
+        $link->level = $ancestor->level + $descendant->level;
+        $link->save();
+      }
+    }
+  }
+  
+  /**
+   * get the root nodes
+   */
+  public function roots() {
+    
+  }
+
+  /**
+   * returns a numeric representation of this node's depth in the tree
+   */
+  public function level() {
+    return count($this->ancestors()) - 1;
+  }
+
+  /**
+   * returns true if the node has a parent, and false if not
+   */
+  public function is_root() {
+    return ($this->level() === 0);
+  }
+
+  /**
    * returns the root of the current node
-   * @return WaxClosureTree
    */
   public function root() {
-    $path_from_root = $this->path_from_root();
-    return $path_from_root[0];
+    $path_from_root = $this->ancestors()->reset();
   }
   
   /**
    * gets the other nodes that are children of this nodes parent
-   * @return WaxRecordSet
    */
   public function siblings() {
   }
   
   /**
-   * if it doesn't exist, creates the closure table and makes all existing nodes roots
-   *
-   * @return void
+   * if it doesn't exist, creates the closure table and closes all nodes over themselves
    */
   public function syncdb(){
-    $this->closure_table->syncdb();
+    if(get_class($this) == "WaxClosureTree") return;
+    parent::syncdb();
+    $this->closure_table()->syncdb();
   }
   
+  /**
+   * close each entry over itself
+   */
   public function after_insert(){
-    $this->closure_table->clear();
-    $this->closure_table->ancestor = $this;
-    $this->closure_table->descendant = $this;
-    $this->closure_table->save();
+    $new_closure = $this->closure_table();
+    $new_closure->ancestor = $this;
+    $new_closure->descendant = $this;
+    $new_closure->level = 0;
+    $new_closure->save();
   }
 }
 ?>
