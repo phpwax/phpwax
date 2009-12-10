132bb67a2466eb9d96362de96d496d72a0863a93
Ross Riley <ross@oneblackbear.com>
2009-12-10 12:30:50 +0000

    The Big Code Cleanup: Rewrite of controller - more slimline

--- dispatch/WaxController.php
+++ dispatch/WaxController.php
@@ -8,8 +8,12 @@
 
 class WaxController
 {
-
-  protected $route_array=null;
+  
+  // A reference to the master application object
+  public $application = false;
+  public $response = false;
+  
+  public $route_array=null;
   public $controller;
   public $action;
   public $use_layout='application';
@@ -23,18 +27,19 @@ class WaxController
 	public $filters = array(); 
 	public $plugins = array();
   //with this you can override the cache settings and turn it of on the application level ignoring the config
-  public $use_cache = true; 
+  public $use_cache = true;
 
-	public function __construct($run_init=true) {
-	  if($run_init) $this->init();    
+	public function __construct($application=false) {
+	  if($application) $this->application = $application;
+	  $this->init();    
   }
   
   public function init(){
     $this->class_name=get_class($this);
-	  $this->set_referrer();
     $this->referrer=Session::get('referrer');
     $this->filters["before"]=array();
-    $this->filters["after"]=array();    
+    $this->filters["after"]=array();
+    $this->response = $this->application->response;
   }
   
   
@@ -44,18 +49,19 @@ class WaxController
 	 *	@access protected
 	 *	@param string $route
  	 */   
-  public function redirect_to($options, $protocol="http://") {
+  protected function redirect_to($options, $protocol="http://") {
+    
     switch(true) {
       case is_array($options):
         $url = $protocol.$_SERVER['HTTP_HOST'].UrlHelper::url_for($options);
-        header("Location:$url"); exit;
+        $this->response->redirect($url);
         break;
-      case preg_match("/^\w+:\/\/.*/", $options): 
-        header("Location:$options"); exit;
+      case preg_match("/^\w+:\/\/.*/", $options):
+        $this->response->redirect($options);
         break;
       case $options=="back":
         if(!$_SERVER['HTTP_REFERER']) return false;
-        header("Location:{$_SERVER['HTTP_REFERER']}"); exit;
+        $this->response->redirect($_SERVER['HTTP_REFERER']);
         break;
       case is_string($options):
         if(substr($options,0,1)!="/"){
@@ -63,9 +69,10 @@ class WaxController
           $options = $_SERVER['REQUEST_URI'] . $options;
         }
         $url = $protocol.$_SERVER['HTTP_HOST'].$options;
-        header("Location:$url"); exit;
+        $this->response->redirect($url);
         break;
     }
+    $this->response->execute();
   }
 
 	public function run_filters($when) {
@@ -75,7 +82,7 @@ class WaxController
         if(!in_array($this->action,$filter[1])) $this->$filter[0]();
       }
       elseif($action == $this->action || $action == "all") {
-          $this->$filter();
+        $this->$filter();
       }
     }
 	}
@@ -96,29 +103,6 @@ class WaxController
     $this->filters["after"][$action]=$action_to_run;
   }
 
-	
-	protected function param($param) {
-	  //if($param=="id") return $this->route_array[0];
-	  return Request::get($param);
-	  if(isset($this->route_array[$param])) return addslashes($this->route_array[$param]);
-	  if(isset($_GET[$param])) return addslashes($_GET[$param]);
-	  return false;
-	}
-	
-	protected function set_referrer() {
-	  Session::set('referrer', $_SERVER['HTTP_REFERER']);
-	}
-	
-	/**
-   *  Surely it's self-documenting?.
-	 *	@return bool
- 	 */
-	protected function is_public_method($object, $method) {
-    if(!method_exists($object, $method)) return false;
-    $this_method = new ReflectionMethod($object, $method);
-		if($this_method->isPublic()) return true;
-	  return false;
-  }
   
   /**
    *  Adds a plugin to the array.
@@ -135,7 +119,7 @@ class WaxController
    *  Returns a view as a string.
 	 *	@return string
  	 */
-  protected function render_view() {
+  public function render_view() {
     if($this->use_plugin) {
       WaxLog::log("info", "[DEPRECATION] use_plugin in controllers is deprecated, use the add_plugin method intead.");
       $this->add_plugin($this->use_plugin);
@@ -159,7 +143,6 @@ class WaxController
       $view->add_path($path.get_parent_class($this)."/".$this->use_view);
       $view->add_path($path."shared/".$this->use_view);
     }
-    ob_end_clean();
     if($this->use_format) $content = $view->parse($this->use_format, 'views');
 		else $content = $view->parse('html', 'views');
 		return $content;
@@ -169,7 +152,7 @@ class WaxController
    *  Returns a layout as a string.
 	 *	@return string
  	 */
-  protected function render_layout() {
+  public function render_layout() {
 		if(!$this->use_layout) return false;
     $layout = new WaxTemplate($this);
     $layout->add_path(VIEW_DIR."layouts/".$this->use_layout);
@@ -181,115 +164,19 @@ class WaxController
   }
   
   
-  /**
-   *  Returns a partial as a string.
-   *  If it exists this method will also run a custom method to initialise
-	 *	@return string
- 	 */
-  public function render_partial($path) {
-	  if(strpos($path, "/")) {
-	    $partial = substr($path, strrpos($path, "/")+1);
-	    $path = substr($path, 0, strrpos($path, "/")+1);
-	    $path = $path."_".$partial;
-	  }else{
-	    $partial = $path;
-	    $path = "_".$path;
-	  }
-		if($this->is_public_method($this, $partial."_partial")) $this->{$partial."_partial"}();
-	  $partial= $this->build_partial($path);		
-		return $partial;
-	}
-	
-	public function build_partial($path, $format = false) {
-	  $partial = new WaxTemplate($this);
-    $partial->add_path(VIEW_DIR.$path);
-    $partial->add_path(VIEW_DIR.$this->controller."/".$path);
-    
-    $partial->add_path(PLUGIN_DIR.$this->use_plugin."/view/".get_parent_class($this)."/".$path);
-    $partial->add_path(PLUGIN_DIR.$this->use_plugin."/view/".$this->plugin_share."/".$path);
-    $partial->add_path(PLUGIN_DIR.$this->share_plugin."/view/".get_parent_class($this)."/".$path);
-    $partial->add_path(PLUGIN_DIR.$this->share_plugin."/view/".$this->plugin_share."/".$path);
-    if($format) $this->use_format = $format;
-    return $partial->parse($this->use_format, "partial");
-	}
-	
-	public function execute_partial($path, $format = false) {
-	  if(strpos($path, "/")) {
-	    $partial = substr($path, strrpos($path, "/")+1);
-	    $path = substr($path, 0, strrpos($path, "/")+1);
-	    $path = $path.$partial;
-	  } else $partial = $path;
-	  if($this->is_public_method($this, $partial)) $this->{$partial}();
-	  $partial= $this->build_partial($partial, $format);		
-		return $partial;
-	}
-	
-	
-	/**
-   *  Returns a single path as a string.
-	 *	@return string
- 	 */
-	public function view_to_string($view_path, $values=array(), $suffix="html") {
-		$view= new WaxTemplate($values);
-		$view->add_path(VIEW_DIR.$view_path);
-		if($this->use_format) return $view->parse($this->use_format);
-		return $view->parse($suffix);
-	}
+  public function is_public_method($object, $method) {
+    return WaxApplication::is_public_method($object, $method);
+  }
+
 
 	/**
  	 *	In the abstract class this remains empty. It is overridden by the controller,
 	 *	any commands will be run by all actions prior to running the action.
 	 *	@access protected
  	 */
-   protected function controller_global() {}
+   public function controller_global() {}
 
 
-	/**
- 	 *	This method is what it's all about, it simply steps through the filters and
-	 *	runs the action.
-	 *
-	 *	It then picks up the view content along the way and hey presto, you have a page.
-	 *  If you've messed up and not provided an action, it throws an exception.
-	 *
-	 *	@access protected
- 	 */
-  public function execute_request() {
-	  $this->controller = WaxUrl::get("controller");    
-	  $this->action = WaxUrl::get("action");
-	  $this->route_array = explode("/", $_GET["route"]);
-	  $this->use_format = WaxUrl::get("format");
-	  WaxLog::log("info", "Loading controller {$this->controller} with action {$this->action} from route '{$_GET['route']}'");
-	  $this->controller_global();
-	  $this->run_filters("before");
-	  if(!$this->is_public_method($this, $this->action)) {
-	    if($this->is_public_method($this, Inflections::underscore($this->action))) {
-	      $underscore_action = Inflections::underscore($this->action);
-	      $this->{$underscore_action}();
-	    } elseif(method_exists($this, 'method_missing') ) {
-			  $this->method_missing();
-		  } else {  	    
-		    $class=get_class($this);
-			  throw new WXRoutingException("No Public Action Defined for - ".$this->action." in controller {$class}.", "Missing Action");
-  		}
-		} else {
-		  $this->{$this->action}();
-		}
-		$this->run_filters("after");		
-		$this->content_for_layout = $this->render_view();
-	  //if(!headers_sent()) header("Content-Type: $type; charset=utf-8");
-		if($content = $this->render_layout()) echo $content;
-		elseif($this->content_for_layout) echo $this->content_for_layout;
-		else echo "";
-	}
-	
-
-  public function is_viewable($path, $format="html"){
-		$file_path = VIEW_DIR . $path . ".". $format;
-		if(is_readable($file_path)) return true;
-		else return false;
-	}
-	
-	
 
 }
 
