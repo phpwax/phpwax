94c2eeb5c7f091a07ec09cd61a9ef7669c29bee9
Ross Riley <ross@oneblackbear.com>
2010-02-26 17:14:08 +0000

    Added filtering support to request helpers

--- helpers/RequestHelper.php
+++ helpers/RequestHelper.php
@@ -3,17 +3,23 @@
 class RequestHelper extends WXHelpers {
   
   
-  public function get($name) {
-    return Request::get($name);
+  public function get($name, $clean=false) {
+    return Request::get($name, $clean);
   }
   
-  public function post($name) {
-    return Request::post($name);
+  public function post($name, $clean=false) {
+    return Request::post($name, $clean);
   }
   
   public function param($name) {
     return Request::param($name);
   }
+  
+  public function filter($name) {
+    return Request::filter(Request::param($name));
+  }
+  
+  
   
   
 }
