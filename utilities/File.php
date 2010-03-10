7bc845d15a18dbc01f24b263d0fc45c555b74acd
Ross Riley <ross@oneblackbear.com>
2010-03-10 10:34:42 +0000

    Added check for dotfile in recursive directory delete

--- utilities/File.php
+++ utilities/File.php
@@ -192,7 +192,7 @@ class File {
 		$iter = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($item), 2);
 		foreach ( $iter as $file ) {
 				if($iter->isDir() && is_readable($file) && !$iter->isDot()) { rmdir($file); }
-				elseif(is_readable($file)) unlink($file);
+				elseif($iter->isFile() && is_readable($file)) unlink($file);
 		}
 		if(is_dir($item) && is_readable($item) && substr($item, -1) != "." ) { rmdir($item); }
 		return true;
