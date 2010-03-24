56863a423f9265225ccad14f4701bf52c573d6ac
Ross Riley <ross@oneblackbear.com>
2010-03-24 10:37:02 +0000

    temp revert of helper rename

--- AutoLoader.php
+++ AutoLoader.php
@@ -256,7 +256,7 @@ class AutoLoader
 
 	static public function register_helpers($classes = array()) {
 	  if(!count($classes)) $classes = get_declared_classes();
-	  foreach($classes as $class) {
+	  foreach((array)$classes as $class) {
 	    if(is_subclass_of($class, "WaxHelpers") || is_subclass_of($class, "WXHelpers") || $class=="WaxHelpers") {
 	      foreach(get_class_methods($class) as $method) {
 	        if(substr($method,0,1)!="_" && !function_exists($method)) WaxCodeGenerator::new_helper_wrapper($class, $method);
