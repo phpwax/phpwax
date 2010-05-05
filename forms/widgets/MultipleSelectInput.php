8d92aca704f4fa1c9f68e8bdefbdbd0e5e047627
Mike <michal.noskovic@googlemail.com>
2010-05-05 13:57:36 +0100

    fix to the multipleselectinput

--- forms/widgets/MultipleSelectInput.php
+++ forms/widgets/MultipleSelectInput.php
@@ -10,8 +10,9 @@ class MultipleSelectInput extends SelectInput {
 
   public $template = '<select multiple="multiple" %s>%s</select>';
 
-  
-
+  public function output_name() {
+		return parent::output_name()."[]";
+	}
 
 
 } // END class
