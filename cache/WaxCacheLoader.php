360bd351a56c8a4c39e39ab49ddb8f54234f6ebd
charlesmarshall <cm56marshall@googlemail.com>
2010-07-26 16:31:37 +0100

    whitespace

--- cache/WaxCacheLoader.php
+++ cache/WaxCacheLoader.php
@@ -2,13 +2,13 @@
 /**
 	* @package PHP-Wax
   */
- 
+
 /**
  *	Class for loading in pre existing cache files based on the current location or value passed in
  *  @package PHP-Wax
  */
 class WaxCacheLoader {
- 
+
 	public $lifetime = 3600;
 	public $config = array();
 	public $engine_type = 'File';
@@ -16,27 +16,27 @@ class WaxCacheLoader {
 	public $dir = false;
   public $identifier = false;
   public $enabled= true;
-  
-  
+
+
   public function __construct($engine="File",$dir="", $lifetime=3600, $format='html'){
     if($engine) $this->engine_type = $engine;
     if($dir) $this->dir = $dir;
     else $this->dir = CACHE_DIR;
-    
-    $this->lifetime = $lifetime;  
+
+    $this->lifetime = $lifetime;
     if(!is_readable($this->dir)) mkdir($this->dir, 0777, true);
     $this->suffix = $format.'.'.$this->suffix;
   }
- 
+
 	public function identifier(){
 	  if(!$this->identifier){
 		  $class = 'WaxCache'.$this->engine_type;
       $engine = new $class($this->dir, $this->lifetime, $this->suffix);
-      $this->identifier =  $engine->make_identifier();      
+      $this->identifier =  $engine->make_identifier();
     }
     return $this->identifier;
 	}
- 
+
   public function excluded($config, $match=false){
     if(!$match) $match = $_SERVER['REQUEST_URI'];
     if($config['exclude_post'] == "yes" && count($_POST)) return true;
@@ -45,15 +45,15 @@ class WaxCacheLoader {
       $all_matches = array();
 	    if(is_array($excluded)){
 	      foreach($excluded as $name => $regex){
-	        preg_match_all($regex, $match, $matches);	      
+	        preg_match_all($regex, $match, $matches);
 	        if(count($matches[0])) $all_matches = array_merge($all_matches, $matches);
-	      }	    
-	    }else preg_match_all($excluded, $match, $all_matches);	    	    
+	      }
+	    }else preg_match_all($excluded, $match, $all_matches);
 	    if(count($all_matches)) return true;
     }
     return false;
   }
-  
+
   public function included($config, $match=false){
     if(!$match) $match = $_SERVER['REQUEST_URI'];
     if($config['exclude_post'] == "yes" && count($_POST)) return false;
@@ -62,50 +62,50 @@ class WaxCacheLoader {
       $all_matches = array();
 	    if(!is_array($included)) $included = array($included);
 	    foreach($included as $name => $regex){
-	      preg_match_all($regex, $match, $matches);	      
+	      preg_match_all($regex, $match, $matches);
 	      if(count($matches[0])) $all_matches = array_merge($all_matches, $matches);
-	    }	   
- 
+	    }
+
       if(!count($all_matches)) return false;
     }
     return true;
   }
-  
+
   public function get(){
     $class = 'WaxCache'.$this->engine_type;
     $engine = new $class($this->dir, $this->lifetime,$this->suffix, $this->identifier);
     return $engine->get();
   }
-  
+
   public function set($value){
     $class = 'WaxCache'.$this->engine_type;
     $engine = new $class($this->dir, $this->lifetime, $this->suffix, $this->identifier);
     return $engine->set($value);
   }
-  
+
   public function expire(){
     $class = 'WaxCache'.$this->engine_type;
     $engine = new $class($this->dir, $this->lifetime, $this->suffix,$this->identifier);
     return $engine->expire();
-  }  
-  
+  }
+
   public function valid($config, $format="html"){
-    $class = 'WaxCache'.$this->engine_type;    
+    $class = 'WaxCache'.$this->engine_type;
     $engine = new $class($this->dir, $this->lifetime, $this->suffix, $this->identifier);
     if(!$this->excluded($config) && $this->included($config) && $engine->get()) return $engine->get();
     else return false;
   }
-  
+
   /**
    * this is whats ran outside of the framework
-   * @param string $config 
+   * @param string $config
    * @return void
    */
   public function layout_cache_loader($config, $format="html"){
-    $this->identifier = $this->identifier();    
-    return $this->valid($config, $format);    
+    $this->identifier = $this->identifier();
+    return $this->valid($config, $format);
   }
-  
+
 }
- 
+
 ?>
