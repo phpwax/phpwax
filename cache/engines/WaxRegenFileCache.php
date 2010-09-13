41fe819a180575d5465d515af3c659af74bf6884
charlesmarshall <cm56marshall@googlemail.com>
2010-09-13 17:17:02 +0100

    trimming off the random stuff

--- cache/engines/WaxRegenFileCache.php
+++ cache/engines/WaxRegenFileCache.php
@@ -1,6 +1,6 @@
 <?
 class WaxRegenFileCache{
-	
+
 	public function __construct($data_file){
 		$config = unserialize(file_get_contents($data_file));
 		$post = unserialize($config['post']);
@@ -8,28 +8,29 @@ class WaxRegenFileCache{
 		if(count($post)==0){
 			touch($config['lock']);
 			if($content = $this->curl($url) ){
+			  if(($start = strpos($content, "<") !== false) && ($end = strrpos($content, ">")) $content = substr($content, $start, ($end - $start));
 				file_put_contents($config['ident'], $content);
 				$config['time'] = time();
-				$config['regen'] = date("Y-m-d H:i:s");			
-				file_put_contents($data_file, serialize($config));				
+				$config['regen'] = date("Y-m-d H:i:s");
+				file_put_contents($data_file, serialize($config));
 			}
 			unlink($config['lock']);
 		}
 	}
-	
+
 	public function curl($url){
     $headers = array(	"Content-Type: application/html; charset=UTF-8", "Accept: application/html; charset=UTF-8");
 		$session = curl_init($url);
 		curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
 		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($session, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($session, CURLOPT_USERAGENT, API_USER_AGENT);		
-		$exec =  curl_exec($session);		
+		curl_setopt($session, CURLOPT_USERAGENT, API_USER_AGENT);
+		$exec =  curl_exec($session);
 		$info = curl_getInfo($session);
 		if($info['http_code'] == 200) return $exec;
 		return false;
   }
-	
+
 	public function parse_location($url){
 		$parsed = parse_url($url);
 		return $parsed['scheme']."://".$parsed['host'].$parsed['path']."?no-wax-cache=1&".$parsed['query'];
