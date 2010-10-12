14c1e011c80b758c4a9aa90ed64d6f33fbf2c7e3
charlesmarshall <cm56marshall@googlemail.com>
2010-10-12 15:46:39 +0100

    fixes

--- cache/engines/WaxRegenFileCache.php
+++ cache/engines/WaxRegenFileCache.php
@@ -5,7 +5,7 @@ class WaxRegenFileCache{
 		$config = unserialize(file_get_contents($data_file));
 		$post = unserialize($config['post']);
 		$url = $this->parse_location($config['location']);
-		if(count($post)==0 && !is_file($config['touch'])){
+		if(count($post)==0 && !is_file($config['lock'])){
 			touch($config['lock']);
 			if($content = $this->curl($url) ){
 			  $start = strpos($content, "<");
@@ -40,7 +40,7 @@ class WaxRegenFileCache{
 
 
 if(isset($argv)){
-	foreach($argv as $file) if(is_readable($file) && strstr($file, "tmp")) $cache = new WaxRegenFileCache($file);
+	foreach($argv as $file) if(is_readable($file) && !strstr($file, ".php")) $cache = new WaxRegenFileCache($file);
 }
 
 ?>
