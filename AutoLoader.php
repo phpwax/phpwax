6dd5377000a465fb5b00d13e475be974c3601525
charlesmarshall <cm56marshall@googlemail.com>
2010-10-20 02:20:28 +0100

    make sure engine is set for images as well

--- AutoLoader.php
+++ AutoLoader.php
@@ -65,7 +65,7 @@ function auto_loader_check_cache(){
     }
   }  
   /** ALSO CHECK FOR IMAGES **/
-  if($img_config = Config::get('image_cache') && substr_count($_SERVER['REQUEST_URI'], 'show_image')){
+  if(($img_config = Config::get('image_cache')) && substr_count($_SERVER['REQUEST_URI'], 'show_image') && $img_config['engine']){
 		if($img_config['include_path']) include_once WAX_ROOT .$img_config['include_path'] .'WaxCache'.$img_config['engine'].'.php'; 
 		else include_once FRAMEWORK_DIR .'/cache/engines/WaxCache'.$img_config['engine'].'.php';
     if(isset($img_config['lifetime'])) $cache = new WaxCacheLoader($img_config['engine'], $image_cache_location, $img_config['lifetime']);
