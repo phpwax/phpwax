d76097741a0edeb8198a2cbdfa47693016aa91ed
charlesmarshall <cm56marshall@googlemail.com>
2010-10-27 17:11:42 +0100

    use pre hooks to replace the cache functions etc that were there before, more extendable

--- WaxAutoLoader.php
+++ WaxAutoLoader.php
@@ -4,7 +4,7 @@
  *  Sets up constants for the main file locations.
  *  @package PHP-Wax
  */
- 
+
 /**
  * Custom iterator - excludes any git files, hidden config files etc
  */
@@ -15,8 +15,70 @@ class WaxRecursiveDirectoryIterator extends RecursiveDirectoryIterator {
   }
 }
 
-class WaxAutoLoader{
+/**
+ * WaxCacheTrigger to look for cache triggers - legacy
+ */
+class WaxCacheTrigger{
+  public static $mime_types = array("json" => "text/javascript", 'js'=> 'text/javascript', 'xml'=>'application/xml', 'rss'=> 'application/rss+xml', 'html'=>'text/html', 'kml'=>'application/vnd.google-earth.kml+xml');
+  public static $yaml = true;
   
+  public function layout(){
+    $cache_location = CACHE_DIR .'layout/';
+    include_once FRAMEWORK_DIR .'/utilities/Session.php';
+    if(WaxCacheTrigger::$yaml) include_once FRAMEWORK_DIR .'/utilities/Spyc.php';
+    include_once FRAMEWORK_DIR .'/utilities/Config.php';
+    include_once FRAMEWORK_DIR .'/cache/WaxCacheLoader.php';
+    include_once FRAMEWORK_DIR .'/interfaces/CacheEngine.php';
+    include_once FRAMEWORK_DIR .'/cache/engines/WaxCacheFile.php';
+    include_once FRAMEWORK_DIR .'/utilities/File.php';
+    
+    if(($config = Config::get('layout_cache')) && $config['engine']){
+      if($_REQUEST['no-wax-cache']) return false;
+  		if($config['include_path']) include_once WAX_ROOT .$config['include_path'] .'WaxCache'.$config['engine'].'.php'; 
+  		else include_once FRAMEWORK_DIR .'/cache/engines/WaxCache'.$config['engine'].'.php';		
+      $cache = new WaxCacheLoader($config, $cache_location);
+
+      if($content = $cache->layout_cache_loader($config)){
+        $url_details = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+        $pos = strrpos($url_details['path'], ".");
+        $ext = substr($url_details['path'],$pos+1); 
+        if(isset($mime_types[$ext])) header("Content-type:".$mime_types[$ext]);
+        header("wax-cache: true");
+        header("wax-cache-eng: ".$config['engine']);
+        header("wax-cache-id: ".str_replace(CACHE_DIR, "", $cache->identifier()));
+        echo $content;
+        exit;
+      }
+    }
+  }
+  
+  public function image(){
+    $image_cache_location = CACHE_DIR .'images/';
+    include_once FRAMEWORK_DIR .'/utilities/Session.php';
+    if(WaxCacheTrigger::$yaml) include_once FRAMEWORK_DIR .'/utilities/Spyc.php';
+    include_once FRAMEWORK_DIR .'/utilities/Config.php';
+    include_once FRAMEWORK_DIR .'/cache/WaxCacheLoader.php';
+    include_once FRAMEWORK_DIR .'/interfaces/CacheEngine.php';
+    include_once FRAMEWORK_DIR .'/cache/engines/WaxCacheFile.php';
+    include_once FRAMEWORK_DIR .'/cache/engines/WaxCacheImage.php';    
+    include_once FRAMEWORK_DIR .'/utilities/File.php';
+    if(($img_config = Config::get('image_cache')) && substr_count($_SERVER['REQUEST_URI'], 'show_image') && $img_config['engine']){
+  		if($img_config['include_path']) include_once WAX_ROOT .$img_config['include_path'] .'WaxCache'.$img_config['engine'].'.php'; 
+  		else include_once FRAMEWORK_DIR .'/cache/engines/WaxCache'.$img_config['engine'].'.php';
+      if(isset($img_config['lifetime'])) $cache = new WaxCacheLoader($img_config['engine'], $image_cache_location, $img_config['lifetime']);
+      else $cache = new WaxCacheLoader('Image', $image_cache_location);
+      if($cache->valid($img_config)) File::display_image($cache->identifier);
+    }
+  }
+}
+/**
+ * Redirection of CMS Assets
+ */
+
+
+
+class WaxAutoLoader{
+
   /**
    * list of all constants to create should look like - sorted by keys
    * - CONSTANT_NAME => array('parent'=>PARENT_CONSTANT, 'value'=>$VALUE, 'function'=>function_name, 'params'=>params_to_pass_to_function);
@@ -28,12 +90,16 @@ class WaxAutoLoader{
   public static $ini_file = "ini.php";
   public static $inis = array();
   /**
-   * some file types should be sniffed for
+   * list of functions that can be called as a pre hook key array - ie remapping certain urls for the cms, adding in cache, etc
+   * First key is the path to the file, that has an array of classes which is an array of functions
+   * /path/to/file/from/wax_root => array('class_name_1'=> array('func_1', 'func_to_call_2'), 'class_name_2'=>array('func_3'))
    */
-  public static $remapped_assets = array();
-  //array of all plugins inside the plugin folder
-  public static $plugins = array();  
+  public static $pre_functions = array();
   
+  
+  //array of all plugins inside the plugin folder
+  public static $plugins = array();
+
   /**
    * register all the constants
    */
@@ -43,7 +109,7 @@ class WaxAutoLoader{
       $parent = ($info['parent']) ? constant($info['parent']) : "";
       if($info['value']) $value = $info['value'];
       elseif($info['function'] && $info['params']) $value = call_user_func($info['function'], $info['params']);
-      elseif($info['function']) $value = call_user_func($info['function']);      
+      elseif($info['function']) $value = call_user_func($info['function']);
       if(!defined($name)) define($name, $parent.$value);
     }
   }
@@ -51,21 +117,37 @@ class WaxAutoLoader{
    * register & include the inis
    */
   public static function inis(){
-    $dir = new RecursiveIteratorIterator( $dirit = new WaxRecursiveDirectoryIterator($directory), true);
+    $dir = new RecursiveIteratorIterator( $dirit = new WaxRecursiveDirectoryIterator(PLUGIN_DIR), true);
     foreach($dir as $file){
       if(basename($file) ==  WaxAutoLoader::$ini_file){
         WaxAutoLoader::$inis[] = $file;
         include_once $file;
       }
     }
+  }  
+  /**
+   * Run over the pre init hooks - cache would a good one
+   */
+  public static function pre_init_hooks(){
+    foreach(WaxAutoLoader::$pre_functions as $path=>$classes){
+      if(is_readable(WAX_ROOT.$path)){
+        include_once WAX_ROOT.$path;
+        foreach($classes as $class=>$functions){
+          $obj = new $class;
+          foreach($functions as $func) $obj->$func();
+        }
+      }
+    }
   }
-    
+  
   /**
    * Main function
    */
   public static function initialise(){
     WaxAutoLoader::constants();
     WaxAutoLoader::inis();
+    //lets you do caching & remapping
+    WaxAutoLoader::pre_init_hooks();
     exit;
   }
 }
@@ -93,9 +175,10 @@ WaxAutoLoader::$wax_constants = array(
                                     'PLUGIN_DIR' => array('parent'=>'WAX_ROOT', 'value'=>'plugins/')
                                     );
 /**
- * to remap a certain file to an alternative location you can use this regex based mapping
- */                                    
-WaxAutoLoader::$
-
+ * Load in the 2 default mapping functions used previously
+ */
+WaxAutoLoader::$pre_functions = array(
+                                    'wax/WaxAutoLoader.php' => array('WaxCacheTrigger'=> array('layout', 'image'))
+                                  );
 
 ?>
