7ade4bab7636a153aea9fd7e54338eb08a5b13fc
charlesmarshall <cm56marshall@googlemail.com>
2012-01-30 19:43:10 +0000

    if the primary fields are not integers, then use the correct field type

--- db/WaxModelJoin.php
+++ db/WaxModelJoin.php
@@ -8,13 +8,13 @@ class WaxModelJoin extends WaxModel {
   public $disallow_sync = true;
   public $left_field = false;
   public $right_field = false;
-  
+
   public function init(WaxModel $left, WaxModel $right) {
     if(!$this->table) $this->table = $left->table."_".$right->table;
-    $this->define($left->table."_".$left->primary_key, "IntegerField");
-    $this->define($right->table."_".$right->primary_key, "IntegerField");
+    $this->define($left->table."_".$left->primary_key, (($left->primary_type == "AutoField") ? "IntegerField" : $left->primary_type) );
+    $this->define($right->table."_".$right->primary_key, (($right->primary_type == "AutoField") ? "IntegerField" : $right->primary_type) );
     $this->left_field = $left->table."_".$left->primary_key;
     $this->right_field = $right->table."_".$right->primary_key;
   }
-  
+
 }
