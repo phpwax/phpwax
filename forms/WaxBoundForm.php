098a696e0eb69627c9792f69197a0cc79574a08d
charlesmarshall <cm56marshall@googlemail.com>
2012-03-27 20:52:30 +0100

    really need to check for post or it breaks many things

--- forms/WaxBoundForm.php
+++ forms/WaxBoundForm.php
@@ -1,16 +1,16 @@
 <?php
 
 class WaxBoundForm implements iterator {
-  
+
   public $post_data = false;
   public $bound_to_model;
   public $elements = array();
   public $errors = array();
   public $prefix;
-  
+
   public function __construct($model, $post_data, $options=array()) {
     foreach($options as $k=>$v) $this->{$k} = $v;
-    if(!$this->prefix) $this->prefix = $model->table; 
+    if(!$this->prefix) $this->prefix = $model->table;
     if(!$post_data &&  $_REQUEST[$this->prefix]) $this->post_data = $_REQUEST[$this->prefix];
     $this->bound_to_model = $model;
     foreach($model->columns as $column=>$options) {
@@ -20,46 +20,47 @@ class WaxBoundForm implements iterator {
       if($element->editable) $this->elements[$column] = $widget;
     }
   }
-  
+
   public function add_element($name, $field_type, $settings=array()) {
     $widget = new $field_type($name, $settings);
     $this->elements[$name] = $widget;
   }
-  
+
   public function save() {
     if(!$this->is_posted()) return false;
     $associations = array();
     foreach($this->elements as $name=>$el){
-      if(!$el->is_association) $this->bound_to_model->{$name} = $el->handle_post($this->post_data[$name]);
-      else $associations[$name] = $el;
+      if(!$el->is_association && isset($this->post_data[$name]) ) $this->bound_to_model->{$name} = $el->handle_post($this->post_data[$name]);
+      else{
+        $alt_name = false;
+        if(($this->bound_to_model->columns[$name][0] == "ForeignKey") && ($t_class = $this->bound_to_model->columns[$name][1]['target_model'])){
+          $alt = new $t_class;
+          $alt_name = $alt->table.'_'.$alt->primary_key;
+          if($this->post_data[$alt_name]) $this->bound_to_model->{$alt_name} = $el->handle_post($this->post_data[$alt_name]);
+        } elseif(isset($this->post_data[$name])) $associations[$name] = $el;
+      }
     }
     foreach($associations as $name=>$el){
-      $alt_name = false;
-      if(($this->bound_to_model->columns[$name][0] == "ForeignKey") && ($t_class = $this->bound_to_model->columns[$name][1]['target_model'])){
-        $alt = new $t_class;
-        $alt_name = $alt->table.'_'.$alt->primary_key;
-        if($this->post_data[$alt_name]) $this->bound_to_model->{$alt_name} = $el->handle_post($this->post_data[$alt_name]);
-      }
-      if(!$alt_name) $this->bound_to_model->{$name} = $el->handle_post($this->post_data[$name]);
+      $this->bound_to_model->{$name} = $el->handle_post($this->post_data[$name]);
     }
     $res = $this->bound_to_model->save();
     $this->validate();
     if($this->is_valid()) return $res;
     return $this->is_valid();
   }
-  
-  public function validate() {  
+
+  public function validate() {
     foreach($this->elements as $el) {
       if(!$el->is_valid()) $this->errors[] = $el->errors;
-    } 
-    
+    }
+
   }
-  
+
   public function is_valid() {
     if( count($this->errors)) return false;
     return true;
   }
-  
+
   public function is_posted(){
     if($this->post_data==false) return false;
     foreach($this->elements as $k=>$el) {
@@ -67,10 +68,10 @@ class WaxBoundForm implements iterator {
     }
     return false;
   }
-  
-  
+
+
   /* Iterator functions */
-   
+
    public function current() {
      return current($this->elements);
    }
@@ -91,5 +92,5 @@ class WaxBoundForm implements iterator {
    public function valid() {
      return $this->current() !== false;
    }
-  
+
 }
