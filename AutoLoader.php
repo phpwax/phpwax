51ae215c83c468aa2cfe9acfd948c46c403c25c3
Ross Riley <riley.ross@gmail.com>
2013-01-24 16:19:54 +0000

    try debug

--- AutoLoader.php
+++ AutoLoader.php
@@ -151,7 +151,6 @@ class AutoLoader
     /*** If this fails, and we aren't initialised, try autoregistering the plugins ****/
     if(!self::$plugins_initialised) {
       self::autoregister_plugins();
-      self::$plugins_initialised = true;
       self::include_from_registry($class_name);
     }
     if(!self::$initialised) {
