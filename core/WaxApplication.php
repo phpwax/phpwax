e9c21cff3aeb1595dbf35857e13f03f38f728845
Ross Riley <riley.ross@gmail.com>
2013-11-23 07:07:25 +0000

    fix a rather obvious bug in environment selection

--- core/WaxApplication.php
+++ core/WaxApplication.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
-	*  @package PHP-Wax  
+	*  @package PHP-Wax
   *  This is essentially a FrontController whose job in life
-  *  is to parse the request and delegate the job to another 
+  *  is to parse the request and delegate the job to another
   *  controller that cares.
   *
   *  In making this decision it will consult the application configuration for guidance.
@@ -11,7 +11,7 @@
   *  the application and setup some kind of Database Connection if required.
   *
   */
-  
+
 class WaxApplication {
 
 
@@ -19,7 +19,7 @@ class WaxApplication {
   public $response = false;
 
   /**
-    *  Step 1. Setup an environment. 
+    *  Step 1. Setup an environment.
     *  Step 2. Find out if we're having a database and set it up.
     *  Step 3. Pass on the work to a delegate controller.
     *
@@ -27,7 +27,7 @@ class WaxApplication {
 
 	public function __construct($delegate) {
     WaxEvent::run("wax.start");
-    $this->setup_environment();	
+    $this->setup_environment();
 	  $this->initialise_database();
 	  if($delegate) $this->execute();
 	  else $this->response = new WaxResponse;
@@ -35,26 +35,26 @@ class WaxApplication {
 
 
   /**
-	 *	Instantiates a config object and constructs the route.  
-	 *  @access private  
-   *  @return void  
+	 *	Instantiates a config object and constructs the route.
+	 *  @access private
+   *  @return void
    **/
-   
+
 	private function setup_environment() {
 		if(defined('ENV')) {
 		  Config::set_environment(ENV);
 		} elseif($_SERVER["ENV"]) {
 			define("ENV", $_SERVER["ENV"]);
-		  Config::set_environment('production');
+		  Config::set_environment($_SERVER["ENV"]);
 		} elseif(Config::get($_SERVER["SERVER_NAME"])) {
 		  Config::set_environment($_SERVER["SERVER_NAME"]);
 		  define("ENV", $_SERVER["SERVER_NAME"]);
 		} else Config::set_environment('production');
-			  
-		//  Looks for an environment specific file inside app/config 
-		if(is_readable(CONFIG_DIR.ENV.".php")) require_once(CONFIG_DIR.ENV.".php");	  
+
+		//  Looks for an environment specific file inside app/config
+		if(is_readable(CONFIG_DIR.ENV.".php")) require_once(CONFIG_DIR.ENV.".php");
   }
-  
+
   /**
 	 *	Instantiates a database connection. It requires PDO which is available in PHP 5.1
 	 *  It then passes this information to the ActiveRecord object.
@@ -64,24 +64,24 @@ class WaxApplication {
 	 *  Host defaults to *localhost*
 	 *  Port defaults to *3306*
 	 *
-	 *  @access private  
+	 *  @access private
    *  @return void
    */
-  
+
   private function initialise_database() {
     if($db = Config::get('db')) {
       if($db['dbtype']=="none") return false;
       WaxModel::load_adapter($db);
     }
   }
-	
+
 
   /**
 	 *	The main application method, triggers all application events.
 	 *  Delegates response handling to WaxResponse.
    *
    **/
-   
+
   public function execute() {
     WaxEvent::run("wax.request");
 	  $this->request = WaxUrl::$params;
@@ -100,25 +100,25 @@ class WaxApplication {
 
 	  WaxEvent::run("wax.controller", $controller);
 	  WaxEvent::run("wax.pre_render", $controller);
-	  if($controller->render !==false) $this->execute_controller($controller);    
+	  if($controller->render !==false) $this->execute_controller($controller);
 		WaxEvent::run("wax.post_render", $this->response);
 		$this->response->execute();
   }
-  
-  
+
+
   /**
 	 *	Takes the controller and triggers the relevant action.
 	 *  @param WaxController $controller
    *
    **/
-  
-  public function execute_controller(&$controller) {	      
+
+  public function execute_controller(&$controller) {
 	  $controller->action = WaxUrl::get("action");
 	  $controller->route_array = explode("/", trim(WaxUrl::$original_route,"/"));
 	  $controller->use_format = WaxUrl::get("format");
-	  
+
     WaxEvent::run("wax.controller_global", $controller);
-	  $controller->controller_global();	  
+	  $controller->controller_global();
     WaxEvent::run("wax.before_filter", $controller);
 	  $controller->run_filters("before");
 	  if(!$this->is_public_method($controller, $controller->action)) {
@@ -128,7 +128,7 @@ class WaxApplication {
 	      $controller->{$underscore_action}();
 	    } elseif(method_exists($controller, 'method_missing') ) {
 			  $controller->method_missing();
-		  } else {  	    
+		  } else {
 		    $class=get_class($controller);
 		    WaxEvent::run("wax.404", $controller);
 			  throw new WXRoutingException("No Public Action Defined for - ".$controller->action." in controller {$class}.", "Missing Action");
@@ -137,24 +137,24 @@ class WaxApplication {
 		  WaxEvent::run("wax.action", $controller);
 		  $controller->{$controller->action}();
 		}
-		
+
 		WaxEvent::run("wax.after_filter", $controller);
-		$controller->run_filters("after");		
+		$controller->run_filters("after");
 		$controller->content_for_layout = $controller->render_view();
 		WaxEvent::run("wax.layout", $controller);
-	  
+
 		if($content = $controller->render_layout()) $this->response->write($content);
 		elseif($controller->content_for_layout) $this->response->write($controller->content_for_layout);
-		else $this->response->write(""); 
+		else $this->response->write("");
   }
-  
+
   //### Application Helper Methods
-  
+
   /**
    *  Surely it's self-documenting?.
 	 *	@return bool
  	 **/
- 	 
+
 	public static function is_public_method($object, $method) {
     if(!method_exists($object, $method)) return false;
     $this_method = new ReflectionMethod($object, $method);
