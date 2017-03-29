20695f309c5d94b215229e751080a1bac0e1c938
Ross Riley <riley.ross@gmail.com>
2017-03-29 11:30:47 +0100

    remove deprecated magic quotes

--- utilities/WaxEmail.php
+++ utilities/WaxEmail.php
@@ -695,13 +695,11 @@ class WaxEmail
             $this->SetError($this->Lang("file_open") . $path);
             return "";
         }
-        $magic_quotes = get_magic_quotes_runtime();
-        set_magic_quotes_runtime(0);
+
         $file_buffer = fread($fd, filesize($path));
         $file_buffer = $this->EncodeString($file_buffer, $encoding);
         fclose($fd);
-        set_magic_quotes_runtime($magic_quotes);
-
+        
         return $file_buffer;
     }
 
