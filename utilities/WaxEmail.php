04236b361fee82d5e459b19c3a81c40a21849efe
Michal Noskovic <michal@oneblackbear.com>
2021-04-15 14:41:09 +0100

    fix preg_replace deprecated /e with sprintf modifier - using preg_replace_callback

--- utilities/WaxEmail.php
+++ utilities/WaxEmail.php
@@ -873,15 +873,29 @@ class WaxEmail
         }
 
         // Replace every high ascii, control and = characters
-        $encoded = preg_replace(
-            '/([\000-\010\013\014\016-\037\075\177-\377])/e',
-            "'='.sprintf('%02X', ord('\\1'))",
+        // $encoded = preg_replace(
+        //     '/([\000-\010\013\014\016-\037\075\177-\377])/e',
+        //     "'='.sprintf('%02X', ord('\\1'))",
+        //     $encoded
+        // );
+        $encoded = preg_replace_callback(
+            '/([\000-\010\013\014\016-\037\075\177-\377])/', 
+            function($match) {  
+                return  '=' . sprintf('%02X', ord($match[1])); 
+            },
             $encoded
         );
         // Replace every spaces and tabs when it's the last character on a line
-        $encoded = preg_replace(
-            "/([\011\040])" . $this->LE . "/e",
-            "'='.sprintf('%02X', ord('\\1')).'" . $this->LE . "'",
+        // $encoded = preg_replace(
+        //     "/([\011\040])" . $this->LE . "/e",
+        //     "'='.sprintf('%02X', ord('\\1')).'" . $this->LE . "'",
+        //     $encoded
+        // );
+        $encoded = preg_replace_callback(
+            '/([\011\040])' . $this->LE . '/', 
+            function($match) {  
+                return  '=' . sprintf('%02X', ord($match[1])) . $this->LE; 
+            },
             $encoded
         );
 
@@ -903,16 +917,37 @@ class WaxEmail
 
         switch (strtolower($position)) {
             case "phrase":
-                $encoded = preg_replace("/([^A-Za-z0-9!*+\/ -])/e", "'='.sprintf('%02X', ord('\\1'))", $encoded);
+                // $encoded = preg_replace("/([^A-Za-z0-9!*+\/ -])/e", "'='.sprintf('%02X', ord('\\1'))", $encoded);
+                $encoded = preg_replace_callback(
+                    '/([^A-Za-z0-9!*+\/ -])/', 
+                    function($match) {  
+                        return  '=' . sprintf('%02X', ord($match[1])); 
+                    },
+                    $encoded
+                );
                 break;
             case "comment":
-                $encoded = preg_replace("/([\(\)\"])/e", "'='.sprintf('%02X', ord('\\1'))", $encoded);
+                // $encoded = preg_replace("/([\(\)\"])/e", "'='.sprintf('%02X', ord('\\1'))", $encoded);
+                $encoded = preg_replace_callback(
+                    '/([\(\)\"])/', 
+                    function($match) {  
+                        return  '=' . sprintf('%02X', ord($match[1])); 
+                    },
+                    $encoded
+                );
             case "text":
             default:
                 // Replace every high ascii, control =, ? and _ characters
-                $encoded = preg_replace(
-                    '/([\000-\011\013\014\016-\037\075\077\137\177-\377])/e',
-                    "'='.sprintf('%02X', ord('\\1'))",
+                // $encoded = preg_replace(
+                //     '/([\000-\011\013\014\016-\037\075\077\137\177-\377])/e',
+                //     "'='.sprintf('%02X', ord('\\1'))",
+                //     $encoded
+                // );
+                $encoded = preg_replace_callback(
+                    '/([\000-\011\013\014\016-\037\075\077‌​\137\177-\377])/', 
+                    function($match) {  
+                        return  '=' . sprintf('%02X', ord($match[1])); 
+                    },
                     $encoded
                 );
                 break;
